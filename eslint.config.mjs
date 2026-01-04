86a42479aa06f02fce440c817150b985b6713867
chaeyn <chaulfe@gmail.com>
2026-01-05 02:38:55 +0900

    chore: electron toolkit에 맞는 prettier, eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,9 @@ export default defineConfig(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
   },
   eslintConfigPrettier
