7630cb15756e4d0d16dc7df618e3a12bd5fb87f3
chaeyn <chaulfe@gmail.com>
2026-01-05 16:11:25 +0900

    chore: 폰트 정의, README 작성

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { useTheme } from '@/shared/lib/useTheme'
 import { GlobalStyle } from './styles/GlobalStyle'
+import styled from 'styled-components'
+import { Font } from '@/shared/config/font'
+
+const Title = styled.h1`
+  ${Font(Font.title1.bold)}
+`
 
 function App() {
   const { theme, toggleTheme } = useTheme()
@@ -9,6 +15,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div>
+        <Title>Clash</Title>
         <button onClick={toggleTheme}>테마 전환</button>
       </div>
     </ThemeProvider>
