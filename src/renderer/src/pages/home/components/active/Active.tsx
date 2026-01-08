959f8642f9c74690ded7736ceb153ffdf8a80336
오용준 <dhdydwns723@gmail.com>
2026-01-08 20:36:24 +0900

    feat: 홈 페이지 UI

--- src/renderer/src/pages/home/components/active/Active.tsx
+++ src/renderer/src/pages/home/components/active/Active.tsx
@@ -1,5 +1,96 @@
 import * as S from "./Active.style";
+import { useState } from "react";
+
+type CommitDay = {
+  id: number;
+  count: number;
+};
+
+const commitDays: CommitDay[] = Array.from({ length: 365 }, (_, i) => ({
+  id: i + 1,
+  count: Math.floor(Math.random() * 10),
+}));
+
+const months: { id: number; commitCount: number }[] = [
+  { id: 1, commitCount: 31 },
+  { id: 2, commitCount: 41 },
+  { id: 3, commitCount: 23 },
+  { id: 4, commitCount: 12 },
+  { id: 5, commitCount: 25 },
+  { id: 6, commitCount: 7 },
+  { id: 7, commitCount: 12 },
+  { id: 8, commitCount: 9 },
+  { id: 9, commitCount: 11 },
+  { id: 10, commitCount: 12 },
+  { id: 11, commitCount: 19 },
+  { id: 12, commitCount: 21 },
+];
+
+const maxCommit = Math.max(...months.map(m => m.commitCount));
 
 export const Active = () => {
-  return <S.ActiveContainer>active</S.ActiveContainer>;
+  const [selectedSort, setSelectedSort] = useState("Github");
+
+  const MaxCommit = Math.max(...commitDays.map(d => d.count));
+
+  const getLevel = (count: number): number => {
+    if (count === 0) return 0;
+
+    const ratio: number = count / MaxCommit;
+
+    const ratioResult = ratio * 10;
+
+    if (ratioResult >= 8) return 4;
+    if (ratioResult >= 6) return 3;
+    if (ratioResult >= 2) return 2;
+    if (ratioResult > 0) return 1;
+    return 0;
+  };
+
+  return (
+    <S.ActiveContainer>
+      <S.TitleBox>
+        <S.Title>내 활동 분석</S.Title>
+        <S.SelectWrapper>
+          <S.Select value={selectedSort} onChange={e => setSelectedSort(e.target.value)}>
+            {["Github", "solved.ac"].map(option => (
+              <S.Option key={option} value={option}>
+                {option}
+              </S.Option>
+            ))}
+          </S.Select>
+          <S.ArrowIcon />
+        </S.SelectWrapper>
+      </S.TitleBox>
+      <S.Line />
+      <S.StreakContainer>
+        <S.StreakBox>
+          <S.StreakTitle>스트릭</S.StreakTitle>
+          <S.GrassBox>
+            <S.Grid>
+              {commitDays.map(day => (
+                <S.Grass key={day.id} $level={getLevel(day.count)} />
+              ))}
+            </S.Grid>
+          </S.GrassBox>
+        </S.StreakBox>
+        <S.StreakBox>
+          <S.StreakTitle>Contributes 변화 추이</S.StreakTitle>
+          <S.GraphBox>
+            <S.Bars>
+              {months.map(({ id, commitCount }) => (
+                <S.BarWrapper key={id}>
+                  <S.BarValue>{commitCount}</S.BarValue>
+
+                  <S.Bar $ratio={commitCount / maxCommit} />
+
+                  <S.BarLabel>{id}월</S.BarLabel>
+                </S.BarWrapper>
+              ))}
+            </S.Bars>
+          </S.GraphBox>
+        </S.StreakBox>
+      </S.StreakContainer>
+    </S.ActiveContainer>
+  );
 };
--- src/renderer/src/pages/home/components/rival/Rival.tsx
+++ src/renderer/src/pages/home/components/rival/Rival.tsx
@@ -1,5 +1,114 @@
 import * as S from "./Rival.style";
 
+type UserStatus = "ONLINE" | "AWAY" | "OFFLINE";
+
+interface RivalUser {
+  name: string;
+  username: string;
+  profile_image: string;
+  active_time: number;
+  using_app: string;
+  status: UserStatus;
+}
+
+interface RivalsProps {
+  user: RivalUser;
+}
+
+interface RivalsResponse {
+  data: {
+    total_count: number;
+    my_rivals: RivalUser[];
+  };
+}
+
+const RivalsData: RivalsResponse = {
+  data: {
+    total_count: 4,
+    my_rivals: [
+      {
+        name: "멧돼지",
+        username: "seunga_418",
+        profile_image: "https://example.com/profile/seunga_418.png",
+        active_time: 21522,
+        using_app: "VSCode",
+        status: "ONLINE",
+      },
+      {
+        name: "채근영",
+        username: "chaeyn",
+        profile_image: "https://example.com/profile/chaeyn.png",
+        active_time: 18340,
+        using_app: "IntelliJ",
+        status: "AWAY",
+      },
+      {
+        name: "한승환",
+        username: "h.7xn",
+        profile_image: "https://example.com/profile/h7xn.png",
+        active_time: 9720,
+        using_app: "Chrome",
+        status: "OFFLINE",
+      },
+      {
+        name: "권대형",
+        username: "gorani",
+        profile_image: "https://example.com/profile/gorani.png",
+        active_time: 14380,
+        using_app: "Notion",
+        status: "ONLINE",
+      },
+    ],
+  },
+};
+
 export const Rival = () => {
-  return <S.RivalContainer>rival</S.RivalContainer>;
+  return (
+    <S.RivalContainer>
+      <S.Title>내 라이벌</S.Title>
+
+      <S.RivalBox>
+        {RivalsData.data.my_rivals.map(user => (
+          <Rivals key={user.username} user={user} />
+        ))}
+      </S.RivalBox>
+    </S.RivalContainer>
+  );
+};
+
+export const Rivals = ({ user }: RivalsProps) => {
+  const getStatus = (status: UserStatus) => {
+    switch (status) {
+      case "ONLINE":
+        return "온라인";
+      case "AWAY":
+        return "자리비움";
+      case "OFFLINE":
+        return "오프라인";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <S.ProfileContainer>
+      <S.ProfileBox>
+        <S.ProfileContent>
+          <S.ProfileIcon />
+          <S.NameBox>
+            <S.ProfileName>{user.name}</S.ProfileName>
+            <S.ProfileMention>@{user.username}</S.ProfileMention>
+          </S.NameBox>
+        </S.ProfileContent>
+        <S.Status $status={user.status}>{getStatus(user.status)}</S.Status>
+      </S.ProfileBox>
+      <S.ActiveBox>
+        <S.UsingAppContainer>
+          <S.UsingApp />
+          <S.UsingAppText>{user.using_app}</S.UsingAppText>
+        </S.UsingAppContainer>
+        <S.ActiveTime $status={user.status}>{user.active_time}</S.ActiveTime>
+      </S.ActiveBox>
+    </S.ProfileContainer>
+  );
 };
