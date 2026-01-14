9944eb3acad7218de1775355ab66df637ee8f9b7
오용준 <dhdydwns723@gmail.com>
2026-01-14 09:41:15 +0900

    feat: 모달 props 설계

---
+++ src/renderer/src/shared/ui/modal/Modal.style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import Close from "@/features/home/assets/home/no.svg";
+import { font } from "@/shared/config/font";
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+export const ModalContainer = styled.div<{ $width: number; $height: number }>`
+  padding: 1.25rem;
+  border-radius: 1.25rem;
+  width: ${({ $width }) => $width}rem;
+  height: ${({ $height }) => $height}rem;
+  background-color: ${({ theme }) => theme.label.disable};
+  box-shadow: 0 0 7px 0 ${({ theme }) => theme.line.normal};
+`;
+
+export const ModalHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ModalTitle = styled.p`
+  ${font.title2.medium}
+`;
+
+export const CloseIcon = styled(Close)``;
+
+export const CloseButton = styled.div`
+  cursor: pointer;
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
