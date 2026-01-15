a968ec9bd0f018459d7285e883951fbe6908c24b
오용준 <dhdydwns723@gmail.com>
2026-01-15 10:00:03 +0900

    feat: 알람 응답 종류에 따른 UI 상태변화

--- src/renderer/src/widgets/topbar/Topbar.tsx
+++ src/renderer/src/widgets/topbar/Topbar.tsx
@@ -1,22 +1,30 @@
-import * as S from "./Topbat.style";
+import * as S from "./Topbar.style";
 import { useState } from "react";
-import { Explain } from "./Topbat.style";
+import { Explain } from "./Topbar.style";
 
 interface TopbarProps {
   onToggleSidebar: () => void;
 }
 
+type AnswerStatus = "YES" | "NO" | "YET";
+
 interface AlamProps {
   name: string;
   mention: string;
   message: string;
+  answer: AnswerStatus;
 }
 
 const alarmInfo: AlamProps[] = [
-  { name: "멧돼지", mention: "seunga_418", message: "상대방이 라이벌을 신청했어요." },
-  { name: "채근영", mention: "chaeyn", message: "상대방이 라이벌을 신청했어요." },
-  { name: "한승환", mention: "h.7xn", message: "상대방이 라이벌을 신청했어요." },
-  { name: "권대형", mention: "gorani", message: "상대방이 라이벌을 신청했어요." },
+  {
+    name: "멧돼지",
+    mention: "seunga_418",
+    message: "상대방이 라이벌을 신청했어요.",
+    answer: "YES",
+  },
+  { name: "채근영", mention: "chaeyn", message: "상대방이 라이벌을 신청했어요.", answer: "YET" },
+  { name: "한승환", mention: "h.7xn", message: "상대방이 라이벌을 신청했어요.", answer: "YES" },
+  { name: "권대형", mention: "gorani", message: "상대방이 라이벌을 신청했어요.", answer: "NO" },
 ];
 
 export const Topbar = ({ onToggleSidebar }: TopbarProps) => {
@@ -26,6 +34,12 @@ export const Topbar = ({ onToggleSidebar }: TopbarProps) => {
   const handleOpen = () => {
     setIsAlamOpen(prev => !prev);
   };
+
+  const handleConfirm = () => {};
+
+  const handleDeny = () => {};
+
+  const handleDelete = () => {};
 
   return (
     <S.TopbarContainer>
@@ -67,6 +81,22 @@ export const Topbar = ({ onToggleSidebar }: TopbarProps) => {
                       </S.NameDiv>
                       <Explain>{alarm.message}</Explain>
                     </div>
+                    {alarm.answer === "YET" ? (
+                      <S.ChoiceBox>
+                        <S.ChoiceButton onClick={handleConfirm}>
+                          <S.ConfirmIcon />
+                        </S.ChoiceButton>
+                        <S.ChoiceButton onClick={handleDeny}>
+                          <S.DenyIcon />
+                        </S.ChoiceButton>
+                      </S.ChoiceBox>
+                    ) : (
+                      <S.ChoiceBox>
+                        <S.ChoiceButton onClick={handleDelete}>
+                          <S.TrashIcon />
+                        </S.ChoiceButton>
+                      </S.ChoiceBox>
+                    )}
                   </S.AlarmBox>
                 ))}
               </S.AlarmContainer>
@@ -74,13 +104,17 @@ export const Topbar = ({ onToggleSidebar }: TopbarProps) => {
           </S.ModalOverlay>
         ) : isAlamOpen && alarmCnt === 0 ? (
           <S.ModalOverlay>
-            <S.ModalContainer>
+            <S.ModalContainer style={{ height: "13rem" }}>
               <S.ModalHeader>
                 <S.ModalTitle>알림</S.ModalTitle>
                 <S.CloseButton onClick={handleOpen}>
                   <S.CloseIcon />
                 </S.CloseButton>
               </S.ModalHeader>
+              <S.NoneAlarm>
+                <S.CryIcon />
+                현재 도착한 알림이 없습니다.
+              </S.NoneAlarm>
             </S.ModalContainer>
           </S.ModalOverlay>
         ) : null}
