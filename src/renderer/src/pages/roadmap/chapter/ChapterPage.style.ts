3ae79ee8f2d30c309320160238f3446aed00383d
한승환 <coderrornotfound@gmail.com>
2026-01-15 16:11:30 +0900

    feat: Question Modal 탑 부분 UI 구현

--- src/renderer/src/pages/roadmap/chapter/ChapterPage.style.ts
+++ src/renderer/src/pages/roadmap/chapter/ChapterPage.style.ts
@@ -169,3 +169,72 @@ export const MissionLabel = styled.span`
   overflow: hidden;
   text-overflow: ellipsis;
 `;
+
+export const ModalTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1.8rem;
+`;
+
+export const ProgressBarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const BarBackground = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 5.5rem;
+  height: 0.3rem;
+  background-color: ${({ theme }) => theme.label.assistive};
+  border-radius: 0.75rem;
+`;
+
+export const BarActive = styled.div<{ $fill: number }>`
+  width: ${({ $fill }) => $fill}%;
+  height: 0.3rem;
+  background-color: ${({ theme }) => theme.primary.normal};
+  border-radius: 0.75rem;
+`;
+
+export const ProgressLabelBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${font.headline2.medium}
+  color: ${({ theme }) => theme.line.normal};
+`;
+
+export const CurrentProgress = styled.span`
+  ${font.headline2.medium}
+  color: ${({ theme }) => theme.primary.normal};
+`;
+
+export const TotalQuestions = styled.span`
+  ${font.headline2.medium}
+  color: ${({ theme }) => theme.line.normal};
+`;
+
+export const QuestionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const QuestionTitle = styled.span`
+  ${font.headline1.medium}
+  color: ${({ theme }) => theme.label.normal};
+  width: 16.25rem;
+  text-overflow: ellipsis;
+  text-align: center;
+`;
+
+export const QuestionPrefix = styled.span`
+  ${font.headline1.medium}
+  color: ${({ theme }) => theme.primary.normal};
+`;
