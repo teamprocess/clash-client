3faeeb1460fbdd5c05a120a358ec18c279e524a5
조상철 <chakanchul2533@gmail.com>
2026-01-18 13:50:38 +0900

    feat: filter option 값을 배열에 담아 관리

--- src/renderer/src/features/shop/ui/filter/Filter.tsx
+++ src/renderer/src/features/shop/ui/filter/Filter.tsx
@@ -1,14 +1,27 @@
 import * as S from "./Filter.style";
 
+const sortOptions = [
+  { label: "최신 순", value: "latest" },
+  { label: "인기 순", value: "popular" },
+  { label: "가격 높은 순", value: "price_desc" },
+  { label: "가격 낮은 순", value: "price_asc" },
+];
+
+const categoryOptions = [
+  { label: "배너", value: "banner" },
+  { label: "이름표", value: "nametag" },
+  { label: "휘장", value: "insignia" },
+];
+
 export const Filter = () => {
   return (
     <S.FilterContainer>
       <S.FilterBox>
         <S.SelectWrapper>
           <S.Select>
-            {["최신 순", "인기 순", "가격 높은 순", "가격 낮은 순"].map(option => (
-              <option key={option} value={option}>
-                {option}
+            {sortOptions.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.label}
               </option>
             ))}
           </S.Select>
@@ -16,9 +29,9 @@ export const Filter = () => {
         </S.SelectWrapper>
         <S.SelectWrapper>
           <S.Select>
-            {["배너", "이름표", "휘장"].map(option => (
-              <option key={option} value={option}>
-                {option}
+            {categoryOptions.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.label}
               </option>
             ))}
           </S.Select>
