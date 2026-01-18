0010e84c3bbbb2bf225595d29cc3e0dbfc2eac4e
조상철 <chakanchul2533@gmail.com>
2026-01-18 13:07:57 +0900

    feat: 상점 ProductCard 및 Filter 컴포넌트 분리

--- src/renderer/src/features/shop/ui/popularity/Popularity.style.ts
+++ src/renderer/src/features/shop/ui/popularity/Popularity.style.ts
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 import { font } from "@/shared/config/font";
-import Cookie from "../../assets/cookie.svg";
-import Token from "../../assets/token.svg";
-import { palette } from "@/shared/config/theme";
 
 export const CategoryContainer = styled.div`
   display: flex;
@@ -27,58 +24,4 @@ export const CardContainer = styled.div`
   margin: 0;
   width: 100%;
   box-sizing: border-box;
-`;
-
-export const ProductCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  gap: 0.625rem;
-  width: 13.5rem;
-  height: 13.75rem;
-  background-color: ${({ theme }) => theme.fill.neutral};
-  border: 1px solid ${({ theme }) => theme.line.neutral};
-  border-radius: 0.25rem;
-  overflow: hidden;
-  cursor: pointer;
-`;
-
-export const ProductInfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0.75rem;
-  align-items: flex-start;
-  gap: 0.25rem;
-  align-self: stretch;
-`;
-
-export const ProductTitle = styled.h3`
-  color: ${({ theme }) => theme.label.normal};
-  ${font.headline2.medium};
-`;
-
-export const PriceBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-export const CookieIcon = styled(Cookie)`
-  margin-left: -0.25rem;
-`;
-export const TokenIcon = styled(Token)`
-  margin-left: -0.25rem;
-`;
-
-export const PriceText = styled.p`
-  color: ${({ theme }) => theme.label.normal};
-  ${font.body.bold};
-  height: 1.4rem;
-`;
-
-export const DiscountText = styled.p`
-  color: ${palette.green[40]};
-  ${font.body.bold};
-  margin-left: 0.25rem;
-  height: 1.4rem;
 `;
--- src/renderer/src/features/shop/ui/popularity/Popularity.tsx
+++ src/renderer/src/features/shop/ui/popularity/Popularity.tsx
@@ -1,6 +1,6 @@
 import * as S from "./Popularity.style";
 import { Product } from "@/features/shop/mocks/popularityProducts";
-import { calculateDiscountedPrice } from "@/features/shop/lib/calculateDiscountedPrice";
+import { ProductCard } from "@/features/shop/ui/card/ProductCard";
 
 interface PopularityProps {
   popularProducts: Product[];
@@ -12,20 +12,13 @@ export const Popularity = ({ popularProducts }: PopularityProps) => {
       <S.CategoryTitle>인기</S.CategoryTitle>
       <S.CardContainer>
         {popularProducts.map(product => (
-          <S.ProductCard key={product.id}>
-            <S.ProductInfoBox>
-              <S.ProductTitle>{product.title}</S.ProductTitle>
-              <S.PriceBox>
-                {product.type === "TOKEN" ? <S.TokenIcon /> : <S.CookieIcon />}
-                <S.PriceText>
-                  {calculateDiscountedPrice(product.price, product.discount)}
-                </S.PriceText>
-                {product.discount !== 0 && (
-                  <S.DiscountText>{`(-${product.discount}%)`}</S.DiscountText>
-                )}
-              </S.PriceBox>
-            </S.ProductInfoBox>
-          </S.ProductCard>
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            discount={product.discount}
+            type={product.type}
+          />
         ))}
       </S.CardContainer>
     </S.CategoryContainer>
