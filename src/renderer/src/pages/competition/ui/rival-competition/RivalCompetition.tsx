0c6e822d87f3e5604d9c5d849e592bd7432b2a2f
오용준 <dhdydwns723@gmail.com>
2026-01-19 12:05:33 +0900

    feat: 라이벌과 비교, 드롭다운

--- src/renderer/src/pages/competition/ui/rival-competition/RivalCompetition.tsx
+++ src/renderer/src/pages/competition/ui/rival-competition/RivalCompetition.tsx
@@ -1,59 +1,19 @@
 import * as S from "./RivalCompetition.style";
-import { RivalsResponse } from "@/features/home/model/useHome";
 import { getStatus } from "@/features/home/model/useHome";
-// import { useCompetition } from "@/pages/competition/model/useCompetition";
-
-const RivalsData: RivalsResponse = {
-  data: {
-    my_rivals: [
-      {
-        name: "멧돼지",
-        username: "seunga_418",
-        profile_image: "https://example.com/profile/seunga_418.png",
-        active_time: 21522,
-        using_app: "Visual Studio Code",
-        status: "ONLINE",
-      },
-      {
-        name: "채근영",
-        username: "chaeyn",
-        profile_image: "https://example.com/profile/chaeyn.png",
-        active_time: 18340,
-        using_app: "IntelliJ",
-        status: "AWAY",
-      },
-      {
-        name: "한승환",
-        username: "h.7xn",
-        profile_image: "https://example.com/profile/h7xn.png",
-        active_time: 9720,
-        using_app: "Chrome",
-        status: "OFFLINE",
-      },
-      {
-        name: "권대형",
-        username: "gorani",
-        profile_image: "https://example.com/profile/gorani.png",
-        active_time: 14380,
-        using_app: "Notion",
-        status: "ONLINE",
-      },
-    ],
-  },
-};
+import { useCompetition } from "@/pages/competition/model/useCompetition";
 
 export const RivalCompetition = () => {
-  // const { rivalCompetion } = useCompetition();
+  const { rivalCompetition } = useCompetition();
 
   return (
     <S.Container>
       <S.CompareContentBox>
-        <S.ListContent>
+        <S.ListContent id={"content-1"}>
           <S.RivalList>
             <S.Title>내 라이벌</S.Title>
             <S.Line />
             <S.ProfileWrapper>
-              {RivalsData.data.my_rivals.map(user => (
+              {rivalCompetition.RivalsData.data.my_rivals.map(user => (
                 <>
                   <S.ProfileContainer key={user.username}>
                     <S.ProfileContent>
@@ -65,8 +25,12 @@ export const RivalCompetition = () => {
                       <S.Status $status={user.status}>{getStatus(user.status)}</S.Status>
                     </S.ProfileContent>
                     <S.PlayTime>
-                      <S.UsingAppText>{user.using_app}</S.UsingAppText>
-                      <S.Point>·</S.Point>
+                      {getStatus(user.status) === "온라인" && (
+                        <>
+                          <S.UsingAppText>{user.using_app}</S.UsingAppText>
+                          <S.Point>·</S.Point>
+                        </>
+                      )}
                       {user.active_time}
                     </S.PlayTime>
                   </S.ProfileContainer>
@@ -75,10 +39,45 @@ export const RivalCompetition = () => {
             </S.ProfileWrapper>
           </S.RivalList>
         </S.ListContent>
-        <S.Content></S.Content>
+        <S.Content id={"content-2"}>
+          <S.RivalCompareWrapper>
+            <S.Title>
+              <p>라이벌과 비교</p>
+              <S.DropDownBox>
+                <S.SelectWrapper>
+                  <S.Select
+                    value={rivalCompetition.competitionPeriodDropDown}
+                    onChange={e => rivalCompetition.setCompetitionPeriodDropDown(e.target.value)}
+                  >
+                    {rivalCompetition.competitionPeriodDropDownValue.map(option => (
+                      <S.Option key={option.key} value={option.key}>
+                        {option.label}
+                      </S.Option>
+                    ))}
+                  </S.Select>
+                  <S.ArrowIcon />
+                </S.SelectWrapper>
+                <S.SelectWrapper>
+                  <S.Select
+                    value={rivalCompetition.competitionDropdown}
+                    onChange={e => rivalCompetition.setCompetitionDropdown(e.target.value)}
+                  >
+                    {rivalCompetition.competitionDropDownValue.map(option => (
+                      <S.Option key={option.key} value={option.key}>
+                        {option.label}
+                      </S.Option>
+                    ))}
+                  </S.Select>
+                  <S.ArrowIcon />
+                </S.SelectWrapper>
+              </S.DropDownBox>
+            </S.Title>
+            <S.Line />
+          </S.RivalCompareWrapper>
+        </S.Content>
       </S.CompareContentBox>
       <S.ContentBox>
-        <S.Content></S.Content>
+        <S.Content id={"content-3"}></S.Content>
       </S.ContentBox>
     </S.Container>
   );
