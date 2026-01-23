b273e8d0b4a13c4c2a836565bbcfd9a2e029036a
오용준 <dhdydwns723@gmail.com>
2026-01-23 16:18:16 +0900

    fix: 타입 정의

--- src/renderer/src/entities/competition/api/rival-competition/battleApi.ts
+++ src/renderer/src/entities/competition/api/rival-competition/battleApi.ts
@@ -1,19 +1,21 @@
-import { api } from "@/shared/api";
+import { api, ApiResponse } from "@/shared/api";
 import {
-  BattleData,
-  BattleDetailData,
+  BattleResponse,
+  BattleDetailResponse,
 } from "@/entities/competition/model/rival-competition/battle.types";
 
 export const battleApi = {
   // 전체 배틀 정보 조회
   getBattleInfo: async () => {
-    const result = await api.get<BattleData>("/compete/rivals/battles");
+    const result = await api.get<ApiResponse<BattleResponse>>("/compete/rivals/battles");
     return result.data;
   },
 
   // 배틀 상세 정보 조회
   getBattleDetailInfo: async (id: number) => {
-    const result = await api.get<BattleDetailData>(`/compete/rivals/battles/${id}`);
+    const result = await api.get<ApiResponse<BattleDetailResponse>>(
+      `/compete/rivals/battles/${id}`
+    );
     return result.data;
   },
 };
