4b249163e952609c9f55d62295fd34f94a81466a
오용준 <dhdydwns723@gmail.com>
2026-01-27 21:40:42 +0900

    feat: Compare API 연동

--- src/renderer/src/features/home/ui/compare/Compare.tsx
+++ src/renderer/src/features/home/ui/compare/Compare.tsx
@@ -1,10 +1,11 @@
 import * as S from "./Compare.style";
 import { Link } from "react-router-dom";
 import { Github } from "@/features/home/ui/compare/github/CompareGithub";
-// import { SolvedAc } from "@/features/home/ui/compare/solved-ac/CompareSolvedAc";
 import { CompareProps } from "@/features/home/model/useCompare";
 
-export const Compare = ({ compareDropdown, setCompareDropdown, statsData }: CompareProps) => {
+export const Compare = ({ compareData }: CompareProps) => {
+  if (!compareData) return null;
+
   return (
     <S.Wrapper>
       <S.Container>
@@ -15,23 +16,25 @@ export const Compare = ({ compareDropdown, setCompareDropdown, statsData }: Comp
             </Link>
             <S.Title>어제와 비교</S.Title>
           </S.TitleBox>
-          <S.SelectWrapper>
-            <S.Select value={compareDropdown} onChange={e => setCompareDropdown(e.target.value)}>
-              {["Github", "solved.ac"].map(option => (
-                <S.Option key={option} value={option}>
-                  {option}
-                </S.Option>
-              ))}
-            </S.Select>
-            <S.ArrowIcon />
-          </S.SelectWrapper>
+          {/*<S.SelectWrapper>*/}
+          {/*  <S.Select value={compareDropdown} onChange={e => setCompareDropdown(e.target.value)}>*/}
+          {/*    {["Github", "solved.ac"].map(option => (*/}
+          {/*      <S.Option key={option} value={option}>*/}
+          {/*        {option}*/}
+          {/*      </S.Option>*/}
+          {/*    ))}*/}
+          {/*  </S.Select>*/}
+          {/*  <S.ArrowIcon />*/}
+          {/*</S.SelectWrapper>*/}
         </S.TopPositionBox>
-        {compareDropdown === "Github" ? (
-          <Github yesterday={statsData.data.yesterday} today={statsData.data.today} />
-        ) : (
-          // <SolvedAc statsData={statsData} />
-          <>개발 중</>
-        )}
+        <Github yesterday={compareData?.yesterday} today={compareData?.today} />
+
+        {/*{compareDropdown === "Github" ? (*/}
+        {/*  <Github yesterday={statsData.data.yesterday} today={statsData.data.today} />*/}
+        {/*) : (*/}
+        {/*  // <SolvedAc statsData={statsData} />*/}
+        {/*  <>개발 중</>*/}
+        {/*)}*/}
       </S.Container>
     </S.Wrapper>
   );
