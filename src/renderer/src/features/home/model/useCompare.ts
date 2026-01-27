92d9a41ded5a8604f48cd898f5dec6e37e4da5dd
오용준 <dhdydwns723@gmail.com>
2026-01-27 21:40:42 +0900

    feat: Compare API 연동

--- src/renderer/src/features/home/model/useCompare.ts
+++ src/renderer/src/features/home/model/useCompare.ts
@@ -1,72 +1,6 @@
-import { useState } from "react";
-
-// ComparePage 데이터 GET
-export const statsData = {
-  data: {
-    yesterday: {
-      date: "2026-01-12",
-      commit: {
-        count: 12,
-        representation_repo: "TeamProcess",
-        add_lines: 420,
-        remove_lines: 180,
-        first_commit: "10:21",
-        last_commit: "23:48",
-      },
-      pull_request: {
-        count: 5,
-        representation_repo: "TeamProcess",
-        merged_count: 4,
-        open_count: 1,
-        closed_count: 0,
-        in_review_count: 3,
-        approved_count: 2,
-        request_count: 1,
-      },
-      issue: {
-        count: 3,
-      },
-      review: {
-        count: 2,
-      },
-    },
-    today: {
-      date: "2026-01-13",
-      commit: {
-        count: 32,
-        representation_repo: "MainSystem",
-        add_lines: 850,
-        remove_lines: 320,
-        first_commit: "9:15",
-        last_commit: "18:30",
-      },
-      pull_request: {
-        count: 8,
-        representation_repo: "MainSystem",
-        merged_count: 6,
-        open_count: 2,
-        closed_count: 0,
-        in_review_count: 4,
-        approved_count: 4,
-        request_count: 0,
-      },
-      issue: {
-        count: 3,
-      },
-      review: {
-        count: 1,
-      },
-    },
-  },
-  message: "어제와 비교한 유저의 깃허브 정보 조회를 성공적으로 완료 했습니다.",
-  success: true,
-};
-
-// 증감률 함수
-export interface GrowthRateProps {
-  yesterday?: number;
-  today?: number;
-}
+import { useEffect, useState } from "react";
+import { CompareResponse } from "@/entities/home/model/useCompare.types";
+import { compareApi } from "@/entities/home/api/compareApi";
 
 export const getGrowthInfo = (yesterday: number, today: number) => {
   const diff = today - yesterday;
@@ -77,27 +11,33 @@ export const getGrowthInfo = (yesterday: number, today: number) => {
 };
 
 export const useCompare = () => {
-  const [compareDropdown, setCompareDropdown] = useState("Github");
+  // const [compareDropdown, setCompareDropdown] = useState("Github");
+
+  const [compareData, setCompareData] = useState<CompareResponse | null>(null);
+
+  useEffect(() => {
+    const fetchCompare = async () => {
+      try {
+        const response = await compareApi.getCompare();
+        if (!response.data) return;
+        setCompareData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCompare();
+  }, []);
 
   return {
     compare: {
-      compareDropdown,
-      setCompareDropdown,
-      statsData: statsData,
+      // compareDropdown,
+      // setCompareDropdown,
+      compareData,
     },
-    github: {
-      yesterday: statsData.data.yesterday,
-      today: statsData.data.today,
-    },
-
-    // 차후 추가될 데이터 내용
-    // solved-ac: {
-    //   statsData: statsData.data,
-    // },
   };
 };
 
 export type CompareReturn = ReturnType<typeof useCompare>;
 export type CompareProps = CompareReturn["compare"];
-export type GithubProps = CompareReturn["github"];
-// export type SolvedAcProps = CompareReturn["solved-ac"];
--- src/renderer/src/features/home/ui/compare/github/CompareGithub.tsx
+++ src/renderer/src/features/home/ui/compare/github/CompareGithub.tsx
@@ -1,8 +1,8 @@
 import * as S from "./CompareGithub.style";
-import { GithubProps } from "@/features/home/model/useCompare";
 import { GrowthRate } from "@/features/home/ui/compare/growth-rate/GrowthRate";
+import { CompareResponse } from "@/entities/home/model/useCompare.types";
 
-export const Github = ({ yesterday, today }: GithubProps) => {
+export const Github = ({ today, yesterday }: CompareResponse) => {
   return (
     <S.ContentBox>
       <S.Content>
@@ -28,22 +28,22 @@ export const Github = ({ yesterday, today }: GithubProps) => {
                 <S.CalculateInfoBox>
                   <S.FireIcon />
                   <S.ExplainText>최다 커밋 레포지토리</S.ExplainText>
-                  <S.RepositorieName>{yesterday.commit.representation_repo}</S.RepositorieName>
+                  <S.RepositorieName>{yesterday.commit.representationRepo}</S.RepositorieName>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.CodeIcon />
                   <S.CodeGap>
-                    하루 동안 <S.PlusText>+{yesterday.commit.add_lines}</S.PlusText> /{" "}
-                    <S.MinusText>-{yesterday.commit.remove_lines}</S.MinusText> lines
+                    하루 동안 <S.PlusText>+{yesterday.commit.addLines}</S.PlusText> /{" "}
+                    <S.MinusText>-{yesterday.commit.removeLines}</S.MinusText> lines
                   </S.CodeGap>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.TimeIcon />
                   <S.ExplainText>
-                    첫커밋 <S.TimeText>{yesterday.commit.first_commit}</S.TimeText> · 마지막 커밋{" "}
-                    <S.TimeText>{yesterday.commit.last_commit}</S.TimeText>
+                    첫커밋 <S.TimeText>{yesterday.commit.firstCommit.slice(0, 10)}</S.TimeText> ·
+                    마지막 커밋 <S.TimeText>{yesterday.commit.lastCommit.slice(0, 10)}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
               </S.CalculateContainer>
@@ -55,7 +55,7 @@ export const Github = ({ yesterday, today }: GithubProps) => {
               <S.GithubInfoBox>
                 <S.GitPRIcon />
                 <S.InfoSubtitleBox>
-                  <S.CountText>{yesterday.pull_request.count}</S.CountText>
+                  <S.CountText>{yesterday.pullRequest.count}</S.CountText>
                   <S.InfoSubtitle>Pull request</S.InfoSubtitle>
                 </S.InfoSubtitleBox>
               </S.GithubInfoBox>
@@ -63,26 +63,24 @@ export const Github = ({ yesterday, today }: GithubProps) => {
                 <S.CalculateInfoBox>
                   <S.FireIcon />
                   <S.ExplainText>최다 PR 레포지토리</S.ExplainText>
-                  <S.RepositorieName>
-                    {yesterday.pull_request.representation_repo}
-                  </S.RepositorieName>
+                  <S.RepositorieName>{yesterday.pullRequest.representationRepo}</S.RepositorieName>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.StatusIcon />
                   <S.ExplainText>
-                    Merged <S.TimeText>{yesterday.pull_request.merged_count}</S.TimeText> · Open{" "}
-                    <S.TimeText>{yesterday.pull_request.open_count}</S.TimeText> · Closed{" "}
-                    <S.TimeText>{yesterday.pull_request.closed_count}</S.TimeText>
+                    Merged <S.TimeText>{yesterday.pullRequest.mergedCount}</S.TimeText> · Open{" "}
+                    <S.TimeText>{yesterday.pullRequest.openCount}</S.TimeText> · Closed{" "}
+                    <S.TimeText>{yesterday.pullRequest.closedCount}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.ReviewIcon $width={1.25} />
                   <S.ExplainText>
-                    리뷰 요청 <S.TimeText>{yesterday.pull_request.in_review_count}</S.TimeText> ·
-                    승인 <S.TimeText>{yesterday.pull_request.approved_count}</S.TimeText> · 변경요청{" "}
-                    <S.TimeText>{yesterday.pull_request.request_count}</S.TimeText>
+                    리뷰 요청 <S.TimeText>{yesterday.pullRequest.inReviewCount}</S.TimeText> · 승인{" "}
+                    <S.TimeText>{yesterday.pullRequest.approvedCount}</S.TimeText> · 변경요청{" "}
+                    <S.TimeText>{yesterday.pullRequest.requestCount}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
               </S.CalculateContainer>
@@ -140,22 +138,22 @@ export const Github = ({ yesterday, today }: GithubProps) => {
                 <S.CalculateInfoBox>
                   <S.FireIcon />
                   <S.ExplainText>최다 커밋 레포지토리</S.ExplainText>
-                  <S.RepositorieName>{today.commit.representation_repo}</S.RepositorieName>
+                  <S.RepositorieName>{today.commit.representationRepo}</S.RepositorieName>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.CodeIcon />
                   <S.CodeGap>
-                    하루 동안 <S.PlusText>{today.commit.add_lines}</S.PlusText> /{" "}
-                    <S.MinusText>-{today.commit.remove_lines}</S.MinusText> lines
+                    하루 동안 <S.PlusText>{today.commit.addLines}</S.PlusText> /{" "}
+                    <S.MinusText>-{today.commit.removeLines}</S.MinusText> lines
                   </S.CodeGap>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.TimeIcon />
                   <S.ExplainText>
-                    첫커밋 <S.TimeText>{today.commit.first_commit}</S.TimeText> · 마지막 커밋{" "}
-                    <S.TimeText>{today.commit.last_commit}</S.TimeText>
+                    첫커밋 <S.TimeText>{today.commit.firstCommit.slice(0, 10)}</S.TimeText> · 마지막
+                    커밋 <S.TimeText>{today.commit.lastCommit.slice(0, 10)}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
               </S.CalculateContainer>
@@ -169,13 +167,13 @@ export const Github = ({ yesterday, today }: GithubProps) => {
                   <S.GithubInfoBox>
                     <S.GitPRIcon />
                     <S.InfoSubtitleBox>
-                      <S.CountText>{today.pull_request.count}</S.CountText>
+                      <S.CountText>{today.pullRequest.count}</S.CountText>
                       <S.InfoSubtitle>Pull request</S.InfoSubtitle>
                     </S.InfoSubtitleBox>
                   </S.GithubInfoBox>
                   <GrowthRate
-                    yesterday={yesterday.pull_request.count}
-                    today={today.pull_request.count}
+                    yesterday={yesterday.pullRequest.count}
+                    today={today.pullRequest.count}
                   />
                 </S.RateBox>
               </S.GrowthBox>
@@ -183,24 +181,24 @@ export const Github = ({ yesterday, today }: GithubProps) => {
                 <S.CalculateInfoBox>
                   <S.FireIcon />
                   <S.ExplainText>최다 PR 레포지토리</S.ExplainText>
-                  <S.RepositorieName>{today.pull_request.representation_repo}</S.RepositorieName>
+                  <S.RepositorieName>{today.pullRequest.representationRepo}</S.RepositorieName>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.StatusIcon />
                   <S.ExplainText>
-                    Merged <S.TimeText>{today.pull_request.merged_count}</S.TimeText> · Open{" "}
-                    <S.TimeText>{today.pull_request.open_count}</S.TimeText> · Closed{" "}
-                    <S.TimeText>{today.pull_request.closed_count}</S.TimeText>
+                    Merged <S.TimeText>{today.pullRequest.mergedCount}</S.TimeText> · Open{" "}
+                    <S.TimeText>{today.pullRequest.openCount}</S.TimeText> · Closed{" "}
+                    <S.TimeText>{today.pullRequest.closedCount}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
                 <S.WidthLine />
                 <S.CalculateInfoBox>
                   <S.ReviewIcon $width={1.25} />
                   <S.ExplainText>
-                    리뷰 요청 <S.TimeText>{today.pull_request.in_review_count}</S.TimeText> · 승인{" "}
-                    <S.TimeText>{today.pull_request.approved_count}</S.TimeText> · 변경요청{" "}
-                    <S.TimeText>{today.pull_request.request_count}</S.TimeText>
+                    리뷰 요청 <S.TimeText>{today.pullRequest.inReviewCount}</S.TimeText> · 승인{" "}
+                    <S.TimeText>{today.pullRequest.approvedCount}</S.TimeText> · 변경요청{" "}
+                    <S.TimeText>{today.pullRequest.requestCount}</S.TimeText>
                   </S.ExplainText>
                 </S.CalculateInfoBox>
               </S.CalculateContainer>
