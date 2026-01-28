48f8d18007a1455000b4ca776c4b69bd2e8c0e3a
seunga418 <seungab77@gmail.com>
2026-01-28 22:27:14 +0900

    feat: 프로필 라이벌 컨테이너 컴포넌트 추가

---
+++ src/renderer/src/pages/profile/components/rival-container/RivalContainer.tsx
@@ -0,0 +1,11 @@
+import * as S from "./RivalBox.style";
+
+const RivalContainer = () => {
+  return (
+    <S.RivalList>
+      <S.RivalBox></S.RivalBox>
+    </S.RivalList>
+  );
+};
+
+export default RivalContainer;
