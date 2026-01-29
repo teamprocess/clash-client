2c4b4bd0aa607516cf1854b597493ce55a7c2f91
seunga418 <seungab77@gmail.com>
2026-01-30 01:02:53 +0900

    feat: 프로필 탭바 UI 추가

--- src/renderer/src/pages/profile/components/github-panel/GithubPanel.style.ts
+++ src/renderer/src/pages/profile/components/github-panel/GithubPanel.style.ts
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  width: 46.25rem;
+  width: 39rem;
   height: 24.375rem;
   border-radius: 1rem;
   background: ${({ theme }) => theme.background.alternative};
   display: flex;
   align-items: flex-end;
   justify-content: flex-end;
-  margin-top: 6.25rem;
+`;
+
+export const Banner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.125rem;
+  margin-left: auto;
+  margin-top: 2.1rem;
 `;
