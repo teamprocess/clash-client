69b0d15b79534bf57ed55a05cc557c20671e2bc4
chaeyn <chaulfe@gmail.com>
2026-01-29 12:08:41 +0900

    fix: Electron 환경에서 서버 통신이 불가능한 문제

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,41 +1,45 @@
 import { resolve } from "path";
 import { readFileSync } from "fs";
-import { defineConfig } from "electron-vite";
+import { defineConfig, loadEnv } from "electron-vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  main: {
-    define: {
-      "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
-    },
-  },
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        "@": resolve("src/renderer/src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    main: {
+      define: {
+        "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
       },
     },
-    plugins: [
-      react(),
-      svgr({
-        svgrOptions: {
-          exportType: "default",
-          ref: true,
-          svgo: false,
-          titleProp: true,
+    preload: {},
+    renderer: {
+      resolve: {
+        alias: {
+          "@": resolve("src/renderer/src"),
         },
-        include: "**/*.svg",
-      }),
-    ],
-    server: {
-      https: {
-        key: readFileSync(resolve(__dirname, "certs/local.clash.kr+3-key.pem")),
-        cert: readFileSync(resolve(__dirname, "certs/local.clash.kr+3.pem")),
       },
-      host: "local.clash.kr",
-      port: 5173,
+      plugins: [
+        react(),
+        svgr({
+          svgrOptions: {
+            exportType: "default",
+            ref: true,
+            svgo: false,
+            titleProp: true,
+          },
+          include: "**/*.svg",
+        }),
+      ],
+      server: {
+        https: {
+          key: readFileSync(resolve(__dirname, "certs/local.clash.kr+3-key.pem")),
+          cert: readFileSync(resolve(__dirname, "certs/local.clash.kr+3.pem")),
+        },
+        host: "local.clash.kr",
+        port: 5173,
+      },
     },
-  },
+  };
 });
