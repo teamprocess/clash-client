30a45e32f02f1f4f8e080cb094c971761cbc2102
seunga418 <seungab77@gmail.com>
2026-01-29 22:21:33 +0900

    feat: GithubPanel 컴포넌트 구조 추가

--- src/renderer/src/pages/profile/components/github-panel/GithubPanel.style.ts
+++ src/renderer/src/pages/profile/components/github-panel/GithubPanel.style.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Background = styled.div``;
+export const Background = styled.div`
+  width: 46.25rem;
+  height: 24.375rem;
+  border-radius: 1rem;
+  background: ${({ theme }) => theme.background.alternative};
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  margin-top: 6.25rem;
+`;
