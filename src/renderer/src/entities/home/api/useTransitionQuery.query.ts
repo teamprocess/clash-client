b378303605817ee7b04039043ec1476068e92e3a
오용준 <dhdydwns723@gmail.com>
2026-02-05 09:25:29 +0900

    refactor: apply Transition useQuery

---
+++ src/renderer/src/entities/home/api/useTransitionQuery.query.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { transitionApi } from "@/entities/home/api/transitionApi";
+
+export const useTransitionQuery = () => {
+  return useQuery({
+    queryKey: ["transition"],
+    queryFn: transitionApi.getTransition,
+  });
+};
