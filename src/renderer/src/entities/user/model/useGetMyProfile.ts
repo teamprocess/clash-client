431b95b43091c86d052c411e50718719d894566f
오용준 <dhdydwns723@gmail.com>
2026-02-05 12:32:43 +0900

    fix: useUser.ts 이름 변경

--- src/renderer/src/entities/user/model/useGetMyProfile.ts
+++ src/renderer/src/entities/user/model/useGetMyProfile.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { authApi } from "../api/authApi";
 
-export const useUser = () => {
+export const useGetMyProfile = () => {
   return useQuery({
     queryKey: ["user"],
     queryFn: async () => {
