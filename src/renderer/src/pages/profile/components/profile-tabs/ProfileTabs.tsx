868df7eceb9a181fd8812efc1f295cd0eab44603
seunga418 <seungab77@gmail.com>
2026-02-05 10:03:26 +0900

    feat: 프로필 탭 전환 렌더링 추가

--- src/renderer/src/pages/profile/components/profile-tabs/ProfileTabs.tsx
+++ src/renderer/src/pages/profile/components/profile-tabs/ProfileTabs.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import * as S from "./ProfileTabs.style";
-import { MenuTabs } from "./menu-tabs/MenuTabs";
+import { MenuTabs, TabKey } from "./menu-tabs/MenuTabs";
 import { GithubStreak } from "./github-streak/GithubStreak";
 import { GithubInfo } from "@/pages/profile/components/profile-tabs/github-info/GithubInfo";
 
 export const ProfileTabs = () => {
+  const [activeTab, setActiveTab] = useState<TabKey>("github");
+
   return (
     <S.Banner>
-      <MenuTabs />
+      <MenuTabs activeTab={activeTab} onChangeTab={setActiveTab} />
+
       <S.Background>
-        <GithubStreak />
-        <GithubInfo />
+        {activeTab === "github" && (
+          <>
+            <GithubStreak />
+            <GithubInfo />
+          </>
+        )}
+
+        {activeTab === "item" && <div />}
+
+        {activeTab === "time" && <div />}
       </S.Background>
     </S.Banner>
   );
