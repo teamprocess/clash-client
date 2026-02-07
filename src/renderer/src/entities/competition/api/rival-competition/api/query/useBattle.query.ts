b3ee1ffd4b1d95c24a5da26339c94e2b12160249
오용준 <dhdydwns723@gmail.com>
2026-02-07 14:59:43 +0900

    fix: Mutation쿼리항목에 invalidation 적용

--- src/renderer/src/entities/competition/api/rival-competition/api/query/useBattle.query.ts
+++ src/renderer/src/entities/competition/api/rival-competition/api/query/useBattle.query.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { battleApi } from "../battleApi";
 import { AnalyzeBattleRequest } from "@/entities/competition/model/rival-competition/battle.types";
 
@@ -33,7 +33,17 @@ export const useBattleListQuery = () => {
 };
 
 export const useCreateBattleMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: battleApi.postCreateBattle,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["battleList"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["battleInfo"],
+      });
+    },
   });
 };
