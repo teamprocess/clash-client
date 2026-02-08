e02d2ece90f69b32438a61cabe96df276ac28a8f
seunga418 <seungab77@gmail.com>
2026-02-08 16:50:46 +0900

    feat: 배경 아이템 클릭 시 TopProfile 배경 프리뷰 추가

--- src/renderer/src/pages/profile/components/top-profile/TopProfile.tsx
+++ src/renderer/src/pages/profile/components/top-profile/TopProfile.tsx
@@ -1,12 +1,50 @@
 import * as S from "./TopProfile.style";
 import ProfileImg from "../../assets/MypageProfile.png";
 
-export const TopProfile = () => {
+type TopProfileProps = {
+  bannerAccentColor?: string;
+  bannerBgImageUrl?: string;
+
+  isBackgroundEditing?: boolean;
+
+  onCancelBackground?: () => void;
+  onSaveBackground?: () => void;
+
+  onEditProfile?: () => void;
+  onLogout?: () => void;
+};
+
+export const TopProfile = ({
+  bannerAccentColor,
+  bannerBgImageUrl,
+  isBackgroundEditing,
+  onCancelBackground,
+  onSaveBackground,
+  onEditProfile,
+  onLogout,
+}: TopProfileProps) => {
   return (
-    <S.Banner>
+    <S.Banner $accent={bannerAccentColor} $bgImage={bannerBgImageUrl}>
       <S.Button>
-        <S.ButtonEdit>수정</S.ButtonEdit>
-        <S.ButtonLogout>로그아웃</S.ButtonLogout>
+        {isBackgroundEditing ? (
+          <>
+            <S.ButtonEdit type="button" onClick={onCancelBackground}>
+              취소
+            </S.ButtonEdit>
+            <S.ButtonLogout type="button" onClick={onSaveBackground}>
+              저장
+            </S.ButtonLogout>
+          </>
+        ) : (
+          <>
+            <S.ButtonEdit type="button" onClick={onEditProfile}>
+              수정
+            </S.ButtonEdit>
+            <S.ButtonLogout type="button" onClick={onLogout}>
+              로그아웃
+            </S.ButtonLogout>
+          </>
+        )}
       </S.Button>
 
       <S.ProfileCard>
