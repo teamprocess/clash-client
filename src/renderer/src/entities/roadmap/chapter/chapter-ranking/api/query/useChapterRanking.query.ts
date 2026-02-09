c3b9bb7fbeb4a1fc0dea915ce00b77665e813379
한승환 <coderrornotfound@gmail.com>
2026-02-09 13:18:17 +0900

    refactor: 기존 useRanking 파일 구조 변경

--- src/renderer/src/entities/roadmap/chapter/chapter-ranking/api/query/useChapterRanking.query.ts
+++ src/renderer/src/entities/roadmap/chapter/chapter-ranking/api/query/useChapterRanking.query.ts
@@ -1,9 +1,13 @@
 import { rankingApi } from "@/entities/roadmap/chapter/chapter-ranking/api/chapterRankingApi";
 import { useQuery } from "@tanstack/react-query";
 
+export const chapterRankingQueryKeys = {
+  list: ["chapterRankings"] as const,
+};
+
 export const useChapterRankingQuery = () => {
   return useQuery({
-    queryKey: ["chapterRankings"],
+    queryKey: chapterRankingQueryKeys.list,
     queryFn: rankingApi.getChapterRankings,
   });
 };
