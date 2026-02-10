6f26b1555445de8a1d24b3bc8a570166db23b669
한승환 <coderrornotfound@gmail.com>
2026-02-10 16:16:51 +0900

    refactor: MajorChoice에 적용

--- src/renderer/src/features/major-choice/ui/choice/MajorChoice.tsx
+++ src/renderer/src/features/major-choice/ui/choice/MajorChoice.tsx
@@ -1,65 +1,72 @@
 import * as S from "./MajorChoice.style";
 import { MajorProps } from "@/features/major-choice/model/useMajorChoice";
+import { ChoiceBox } from "@/features/major-choice/ui/choice-box/ChoiceBox";
+import WebIcon from "../../assets/web.svg";
+import AppIcon from "../../assets/app.svg";
+import ServerIcon from "../../assets/server.svg";
+import AiIcon from "../../assets/ai.svg";
+import GameIcon from "../../assets/game.svg";
 
 export const MajorChoice = ({ selectedMajor, major, isValid, username, onSubmit }: MajorProps) => {
   return (
-    <>
-      <S.ChoiceContainer>
-        <S.ChoiceContents>
-          <S.ChoiceContentsTop>
-            <S.ChoiceTitle>원하는 전공을 선택해주세요!</S.ChoiceTitle>
-            <S.ChoiceDescription>
-              안녕하세요. {username}님. 원하는 전공을 선택하여 주세요.
-              <br />
-              희망하는 전공을 선택하여 나의 전공에 맞는 로드맵을 수행할 수 있습니다.
-            </S.ChoiceDescription>
-          </S.ChoiceContentsTop>
-          <S.ChoiceWrapper>
-            <S.ChoiceTop>
-              <S.ChoiceBox onClick={() => selectedMajor("WEB")} $isSelected={major === "WEB"}>
-                <S.ChoiceItem>
-                  {major === "WEB" ? <S.CheckedIcon /> : <S.NotCheckedIcon />}
-                  <S.WebIcon />
-                  <S.ChoiceText>웹</S.ChoiceText>
-                </S.ChoiceItem>
-              </S.ChoiceBox>
-              <S.ChoiceBox onClick={() => selectedMajor("APP")} $isSelected={major === "APP"}>
-                <S.ChoiceItem>
-                  {major === "APP" ? <S.CheckedIcon /> : <S.NotCheckedIcon />}
-                  <S.AppIcon />
-                  <S.ChoiceText>앱</S.ChoiceText>
-                </S.ChoiceItem>
-              </S.ChoiceBox>
-              <S.ChoiceBox onClick={() => selectedMajor("SERVER")} $isSelected={major === "SERVER"}>
-                <S.ChoiceItem>
-                  {major === "SERVER" ? <S.CheckedIcon /> : <S.NotCheckedIcon />}
-                  <S.ServerIcon />
-                  <S.ChoiceText>서버</S.ChoiceText>
-                </S.ChoiceItem>
-              </S.ChoiceBox>
-            </S.ChoiceTop>
-            <S.ChoiceBottom>
-              <S.ChoiceBox onClick={() => selectedMajor("AI")} $isSelected={major === "AI"}>
-                <S.ChoiceItem>
-                  {major === "AI" ? <S.CheckedIcon /> : <S.NotCheckedIcon />}
-                  <S.AiIcon />
-                  <S.ChoiceText>Ai</S.ChoiceText>
-                </S.ChoiceItem>
-              </S.ChoiceBox>
-              <S.ChoiceBox onClick={() => selectedMajor("GAME")} $isSelected={major === "GAME"}>
-                <S.ChoiceItem>
-                  {major === "GAME" ? <S.CheckedIcon /> : <S.NotCheckedIcon />}
-                  <S.GameIcon />
-                  <S.ChoiceText>게임</S.ChoiceText>
-                </S.ChoiceItem>
-              </S.ChoiceBox>
-            </S.ChoiceBottom>
-          </S.ChoiceWrapper>
-          <S.RoadMapButton disabled={!isValid} onClick={onSubmit}>
-            선택 완료하기
-          </S.RoadMapButton>
-        </S.ChoiceContents>
-      </S.ChoiceContainer>
-    </>
+    <S.ChoiceContainer>
+      <S.ChoiceContents>
+        <S.ChoiceContentsTop>
+          <S.ChoiceTitle>원하는 전공을 선택해주세요!</S.ChoiceTitle>
+          <S.ChoiceDescription>
+            안녕하세요. {username}님. 원하는 전공을 선택하여 주세요.
+            <br />
+            희망하는 전공을 선택하여 나의 전공에 맞는 로드맵을 수행할 수 있습니다.
+          </S.ChoiceDescription>
+        </S.ChoiceContentsTop>
+
+        <S.ChoiceWrapper>
+          <S.ChoiceTop>
+            <ChoiceBox
+              size="sm"
+              selected={major === "WEB"}
+              icon={<WebIcon />}
+              label="웹"
+              onClick={() => selectedMajor("WEB")}
+            />
+            <ChoiceBox
+              size="sm"
+              selected={major === "APP"}
+              icon={<AppIcon />}
+              label="앱"
+              onClick={() => selectedMajor("APP")}
+            />
+            <ChoiceBox
+              size="sm"
+              selected={major === "SERVER"}
+              icon={<ServerIcon />}
+              label="서버"
+              onClick={() => selectedMajor("SERVER")}
+            />
+          </S.ChoiceTop>
+
+          <S.ChoiceBottom>
+            <ChoiceBox
+              size="sm"
+              selected={major === "AI"}
+              icon={<AiIcon />}
+              label="AI"
+              onClick={() => selectedMajor("AI")}
+            />
+            <ChoiceBox
+              size="sm"
+              selected={major === "GAME"}
+              icon={<GameIcon />}
+              label="게임"
+              onClick={() => selectedMajor("GAME")}
+            />
+          </S.ChoiceBottom>
+        </S.ChoiceWrapper>
+
+        <S.RoadMapButton disabled={!isValid} onClick={onSubmit}>
+          선택 완료하기
+        </S.RoadMapButton>
+      </S.ChoiceContents>
+    </S.ChoiceContainer>
   );
 };
