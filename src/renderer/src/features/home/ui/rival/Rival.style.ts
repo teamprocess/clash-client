8e9dc8c605b7bfa3f8d311942a2cbec98e38e0dd
오용준 <dhdydwns723@gmail.com>
2026-02-10 14:57:57 +0900

    refactor: Rival에서 예시 적용

--- src/renderer/src/features/home/ui/rival/Rival.style.ts
+++ src/renderer/src/features/home/ui/rival/Rival.style.ts
@@ -5,7 +5,6 @@ import { palette } from "@/shared/config/theme";
 import VSCode from "../../assets/home/vscode.svg";
 import Profile from "../../assets/home/profile.svg";
 import Plus from "../../assets/home/plus.svg";
-import Search from "../../assets/home/search.svg";
 import Checked from "../../assets/home/check-box.svg";
 
 export const RivalContainer = styled.div`
@@ -216,19 +215,6 @@ export const SearchUsers = styled.input`
 `;
 
 export const PlusIcon = styled(Plus)``;
-export const SearchIconBox = styled.div`
-  color: ${({ theme }) => theme.label.neutral};
-`;
-
-export const SearchIcon = styled(Search)`
-  position: absolute;
-  right: 1rem;
-  top: 43%;
-  transform: translateY(-50%);
-  width: 1.85rem;
-  height: 1.85rem;
-  pointer-events: none;
-`;
 
 export const UserChoiceContainer = styled.div`
   display: flex;
