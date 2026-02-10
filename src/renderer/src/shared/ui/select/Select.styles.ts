be1702075bd473041e1248756814cad47aae2dde
오용준 <dhdydwns723@gmail.com>
2026-02-10 18:44:52 +0900

    refactor: Select 공통 컴포넌트 생성

---
+++ src/renderer/src/shared/ui/select/Select.styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { font } from "@/shared/config/font";
+import Arrow from "@/features/home/assets/home/arrow.svg";
+
+export const SelectWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+`;
+
+export const Select = styled.select`
+  ${font.body.regular};
+  width: 7.5rem;
+  height: 2rem;
+  padding: 0 0.75rem;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.fill.neutral};
+  color: ${({ theme }) => theme.label.normal};
+  border: none;
+  appearance: none;
+  cursor: pointer;
+  background-repeat: no-repeat;
+  background-position: right 0.5rem center;
+  background-size: 0.75rem;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const ArrowIcon = styled(Arrow)`
+  position: absolute;
+  top: 32%;
+  right: 1rem;
+  width: 0.75rem;
+  height: 0.75rem;
+`;
