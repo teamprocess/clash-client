a6be6d1e8aae3e99195c914cc2cfa83c23dcb18c
오용준 <dhdydwns723@gmail.com>
2026-02-13 11:57:11 +0900

    fix: 언어 혼용 수정

--- src/renderer/src/features/home/ui/compare/Compare.tsx
+++ src/renderer/src/features/home/ui/compare/Compare.tsx
@@ -17,28 +17,11 @@ export const Compare = () => {
             </Link>
             <S.Title>어제와 비교</S.Title>
           </S.TitleBox>
-          {/*<S.SelectWrapper>*/}
-          {/*  <S.Select value={compareDropdown} onChange={e => setCompareDropdown(e.target.value)}>*/}
-          {/*    {["Github", "solved.ac"].map(option => (*/}
-          {/*      <S.Option key={option} value={option}>*/}
-          {/*        {option}*/}
-          {/*      </S.Option>*/}
-          {/*    ))}*/}
-          {/*  </S.Select>*/}
-          {/*  <S.ArrowIcon />*/}
-          {/*</S.SelectWrapper>*/}
         </S.TopPositionBox>
         <Github
           yesterday={getCompareData.compareData?.yesterday ?? null}
           today={getCompareData.compareData?.today ?? null}
         />
-
-        {/*{compareDropdown === "Github" ? (*/}
-        {/*  <Github yesterday={statsData.data.yesterday} today={statsData.data.today} />*/}
-        {/*) : (*/}
-        {/*  // <SolvedAc statsData={statsData} />*/}
-        {/*  <>개발 중</>*/}
-        {/*)}*/}
       </S.Container>
     </S.Wrapper>
   );
