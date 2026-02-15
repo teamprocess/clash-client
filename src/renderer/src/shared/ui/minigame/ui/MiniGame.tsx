1a5846e82778eef4d0bada9bc5d4d50c9ca9fb8b
오용준 <dhdydwns723@gmail.com>
2026-02-16 01:06:39 +0900

    feat: 깃허브 연동 디아로그 미니게임 버전

---
+++ src/renderer/src/shared/ui/minigame/ui/MiniGame.tsx
@@ -0,0 +1,26 @@
+import * as S from "./MiniGame.styles";
+import { useMiniGame } from "../model/useMiniGame";
+
+export const MiniGame = () => {
+  const { playerY, obstacleX, score, isGameOver, jump, reset } = useMiniGame();
+
+  const handleAction = () => {
+    if (isGameOver) reset();
+    else jump();
+  };
+
+  return (
+    <S.Container
+      type="button"
+      onClick={handleAction}
+      aria-label={isGameOver ? "Restart mini game" : "Jump"}
+    >
+      <S.Player style={{ transform: `translateY(${playerY}px)` }} />
+      <S.Obstacle style={{ transform: `translateX(${obstacleX}px)` }} />
+
+      <S.Score>Score: {score}</S.Score>
+
+      {isGameOver && <S.GameOver>Game Over · click to restart</S.GameOver>}
+    </S.Container>
+  );
+};
