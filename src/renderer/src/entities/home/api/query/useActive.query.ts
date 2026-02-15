e035c4dbf81fd527d2c7cc08a822861f014669df
오용준 <dhdydwns723@gmail.com>
2026-02-16 02:25:33 +0900

    refactor: Suspense 컴포넌트화

--- src/renderer/src/entities/home/api/query/useActive.query.ts
+++ src/renderer/src/entities/home/api/query/useActive.query.ts
@@ -1,11 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { CategoryType } from "@/entities/home/model/useRanking.types";
 import { activeApi } from "@/entities/home/api/activeApi";
 
 export const useActiveQuery = (category: CategoryType) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["active", category],
     queryFn: () => activeApi.getActive(category),
-    enabled: !!category,
   });
 };
--- src/renderer/src/entities/home/api/query/useRivals.query.ts
+++ src/renderer/src/entities/home/api/query/useRivals.query.ts
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { rivalsApi } from "@/entities/home/api/rivalApi";
 
 export const useRivalListQuery = () => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["rivalList"],
     queryFn: rivalsApi.getRivalList,
   });
