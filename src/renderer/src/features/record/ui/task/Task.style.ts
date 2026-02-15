d574c80a618f8a05bc5ee6a0536d5ae83908d743
chaeyn <chaulfe@gmail.com>
2026-02-15 13:35:46 +0900

    refactor: 과목 박스 x축 패딩 크기 조정

--- src/renderer/src/features/record/ui/task/Task.style.ts
+++ src/renderer/src/features/record/ui/task/Task.style.ts
@@ -23,7 +23,7 @@ export const TaskItem = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 1.25rem 2.5rem;
+  padding: 1.25rem 1.5rem;
   border-bottom: 2px solid ${({ theme }) => theme.fill.alternative};
 `;
 
