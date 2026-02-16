2783f54c9b98750c39f2b56e27885a09595210be
오용준 <dhdydwns723@gmail.com>
2026-02-16 11:59:25 +0900

    refactor: 리렌더링 시에만 Dialog 노출하도록 로직 단순화

--- src/renderer/src/pages/home/HomePage.style.ts
+++ src/renderer/src/pages/home/HomePage.style.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Github from "@/shared/ui/assets/github.svg";
+import { palette } from "@/shared/config/theme";
+import { font } from "@/shared/config/font";
 
 export const HomeContainer = styled.div`
   display: grid;
@@ -9,4 +12,38 @@ export const HomeContainer = styled.div`
   grid-gap: 1.5rem;
   height: 100%;
   width: 100%;
+`;
+
+export const ConnectingContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const GithubIcon = styled(Github)`
+  width: 5rem;
+  height: 5rem;
+  color: ${palette.neutral[99]};
+`;
+
+export const FontBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  gap: 0.25rem;
+`;
+
+export const HugeFont = styled.p`
+  ${font.headline2.bold}
+  color: ${({ theme }) => theme.label.normal};
+`;
+
+export const TinyFont = styled.p`
+  ${font.label.medium}
+  color: ${({ theme }) => theme.label.neutral}
 `;
