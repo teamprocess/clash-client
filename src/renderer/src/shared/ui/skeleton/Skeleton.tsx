259a69e52931d4b8f427abbfbe395aadc5bfd6ff
오용준 <dhdydwns723@gmail.com>
2026-02-17 18:57:23 +0900

    fix: 스켈레톤 UI 수정

--- src/renderer/src/shared/ui/skeleton/Skeleton.tsx
+++ src/renderer/src/shared/ui/skeleton/Skeleton.tsx
@@ -3,8 +3,8 @@ import * as S from "./Skeleton.style";
 export const Skeleton = () => {
   return (
     <S.SkeletonContainer>
-      <S.SkeletonBox $width={70} $height={20} />
-      <S.SkeletonBox $width={70} $height={20} />
+      <S.SkeletonBox $width={80} $height={15} />
+      <S.SkeletonBox $width={40} $height={15} />
     </S.SkeletonContainer>
   );
 };
