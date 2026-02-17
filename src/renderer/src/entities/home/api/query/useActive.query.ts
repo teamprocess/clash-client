046735c5baa0faa637fa1730eb4907fefec91b59
오용준 <dhdydwns723@gmail.com>
2026-02-17 18:37:21 +0900

    fix: 드롭다운 변경 시, fallback 미작동 및 이전 데이터 유지

--- src/renderer/src/entities/home/api/query/useActive.query.ts
+++ src/renderer/src/entities/home/api/query/useActive.query.ts
@@ -1,10 +1,16 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { CategoryType } from "@/entities/home/model/useRanking.types";
 import { activeApi } from "@/entities/home/api/activeApi";
 
+export const activeQueryKeys = {
+  active: (category: CategoryType) => ["active", category] as const,
+};
+
 export const useActiveQuery = (category: CategoryType) => {
-  return useSuspenseQuery({
-    queryKey: ["active", category],
+  return useQuery({
+    queryKey: activeQueryKeys.active(category),
     queryFn: () => activeApi.getActive(category),
+    placeholderData: prev => prev,
+    retry: 1,
   });
 };
