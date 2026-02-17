d9cfd0f8b616981fd7a92c893a6df90b4bd5eeeb
오용준 <dhdydwns723@gmail.com>
2026-02-17 18:32:07 +0900

    fix: Suspense 사용으로 초기 페칭시, 다이얼로그 마운트

--- src/renderer/src/pages/home/HomePage.tsx
+++ src/renderer/src/pages/home/HomePage.tsx
@@ -1,10 +1,26 @@
 import * as S from "./HomePage.style";
 import { Home } from "@/pages/home/Home";
+import { Suspense } from "react";
+import { Dialog } from "@/shared/ui";
 
 export const HomePage = () => {
   return (
     <S.HomeContainer>
-      <Home />
+      <Suspense
+        fallback={
+          <Dialog width={21.5} height={21.5} isOpen={true}>
+            <S.ConnectingContainer>
+              <S.GithubIcon />
+              <S.FontBox>
+                <S.HugeFont>깃허브 계정을 연동 중입니다</S.HugeFont>
+                <S.TinyFont>잠시만 기다려주세요.</S.TinyFont>
+              </S.FontBox>
+            </S.ConnectingContainer>
+          </Dialog>
+        }
+      >
+        <Home />
+      </Suspense>
     </S.HomeContainer>
   );
 };
