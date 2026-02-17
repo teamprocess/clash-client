1e295124ff005e006c7c55b0c4d4a9a7daf72381
오용준 <dhdydwns723@gmail.com>
2026-02-17 18:47:56 +0900

    fix: 필요없는 구조 제거

--- src/renderer/src/pages/home/Home.tsx
+++ src/renderer/src/pages/home/Home.tsx
@@ -1,32 +1,17 @@
-import { Dialog } from "@/shared/ui";
-import * as S from "./Home.style";
 import { Active, Ranking, Rival, Transition } from "@/features/home";
 import { useTransitionQuery } from "@/entities/home";
 import { useMyRivalsQuery } from "@/entities/competition";
-import { Suspense } from "react";
 
 export const Home = () => {
   const transitionQuery = useTransitionQuery();
   const myRivalQuery = useMyRivalsQuery();
 
   return (
-    <Suspense
-      fallback={
-        <Dialog width={21.5} height={21.5} isOpen={true}>
-          <S.ConnectingContainer>
-            <S.GithubIcon />
-            <S.FontBox>
-              <S.HugeFont>깃허브 계정을 연동 중입니다</S.HugeFont>
-              <S.TinyFont>잠시만 기다려주세요.</S.TinyFont>
-            </S.FontBox>
-          </S.ConnectingContainer>
-        </Dialog>
-      }
-    >
+    <>
       <Transition data={transitionQuery.data?.data ?? null} />
       <Rival rivalsData={myRivalQuery.data?.data ?? null} />
       <Active />
       <Ranking />
-    </Suspense>
+    </>
   );
 };
