950ba9b259c1e5fd5ed8fb80581ef974b5bff4b8
오용준 <dhdydwns723@gmail.com>
2026-02-17 18:37:21 +0900

    fix: 드롭다운 변경 시, fallback 미작동 및 이전 데이터 유지

--- src/renderer/src/entities/home/api/query/useRanking.query.ts
+++ src/renderer/src/entities/home/api/query/useRanking.query.ts
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { rankingApi } from "@/entities/home/api/rankingApi";
 import { CategoryType, PeriodType } from "@/entities/home/model/useRanking.types";
 
@@ -7,12 +7,10 @@ export const rankingQueryKeys = {
 };
 
 export const useRankingQuery = (category: CategoryType, period: PeriodType) => {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: rankingQueryKeys.rankings(category, period),
-    queryFn: () =>
-      rankingApi.getRanking({
-        category,
-        period,
-      }),
+    queryFn: () => rankingApi.getRanking({ category, period }),
+    placeholderData: prev => prev,
+    retry: 1,
   });
 };
