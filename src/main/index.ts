3505473903e9284a49e9aba5c19fe74e7b6cc61c
chaeyn <chaulfe@gmail.com>
2026-02-18 13:49:22 +0900

    refactor: main 부트스트랩 초기화 로직 모듈 분리

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,10 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import { electronApp, optimizer } from "@electron-toolkit/utils";
+import { BrowserWindow } from "electron";
 import { AppMonitor } from "./services";
 import { createMainWindow } from "./window";
-import { registerIpcHandlers } from "./ipc";
-import { configureCertificateHandling, registerCspHeaders } from "./security";
+import { configureCertificateHandling } from "./security";
 import { registerQuitHandlers } from "./lifecycle";
-import { consumePendingDeepLink, registerDeepLinkEvents, registerProtocol } from "./deeplink";
+import { consumePendingDeepLink, registerDeepLinkEvents } from "./deeplink";
+import { bootstrapMainProcess } from "./bootstrap";
 
 let mainWindow: BrowserWindow | null = null;
 let appMonitor: AppMonitor | null = null;
@@ -23,37 +22,7 @@ function createWindow(): void {
 configureCertificateHandling();
 registerQuitHandlers({ getAppMonitor: () => appMonitor });
 registerDeepLinkEvents(() => mainWindow);
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  registerProtocol();
-  // Set app user model id for windows
-  electronApp.setAppUserModelId("com.electron");
-
-  // CSP 설정
-  registerCspHeaders();
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on("browser-window-created", (_, window) => {
-    optimizer.watchWindowShortcuts(window);
-  });
-
-  // IPC test
-  ipcMain.on("ping", () => console.log("pong"));
-  registerIpcHandlers({ getAppMonitor: () => appMonitor });
-
-  createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+bootstrapMainProcess({ createWindow, getAppMonitor: () => appMonitor });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
