d34602c0fc051a5e58f93945cd368c4f2b4cbad4
오용준 <dhdydwns723@gmail.com>
2026-02-20 18:17:04 +0900

    refactor: 라이벌 추가 함수 생성 및 사용

---
+++ src/renderer/src/features/home/lib/AddRivals.style.ts
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import { font } from "@/shared/config/font";
+import { palette } from "@/shared/config/theme";
+import Profile from "../../assets/home/profile.svg";
+import Checked from "../../assets/home/check-box.svg";
+
+export const ProfileContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.75rem;
+`;
+
+export const ProfileTagBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.125rem;
+`;
+
+export const ProfileIcon = styled(Profile)`
+  width: 2.25rem;
+  height: 2.25rem;
+`;
+
+export const ProfileName = styled.p`
+  ${font.headline2.medium}
+  color: ${({ theme }) => theme.label.normal};
+`;
+
+export const ProfileMention = styled.p`
+  ${font.label.regular}
+  color: ${({ theme }) => theme.label.alternative};
+`;
+
+export const UserChoiceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 12rem;
+  overflow-y: auto;
+  scrollbar-width: none;
+`;
+
+export const UserChoiceBox = styled.div<{ $isSelected: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0.5rem;
+  border-bottom: 1px solid ${({ theme }) => theme.line.alternative};
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.fill.alternative};
+  }
+`;
+
+export const BottomBox = styled.div`
+  margin-top: 0.9rem;
+`;
+
+export const ButtonBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.75rem;
+`;
+
+export const CloseButton = styled.button`
+  width: 4.25rem;
+  border-radius: 0.625rem;
+  color: ${palette.neutral[97]};
+  padding: 0.4rem 0.5rem;
+  background: ${({ theme }) => theme.line.normal};
+  cursor: pointer;
+`;
+
+export const OkayButton = styled.button`
+  width: 4.25rem;
+  border-radius: 0.625rem;
+  color: ${palette.neutral[97]};
+  padding: 0.4rem 0.5rem;
+  background: ${({ theme }) => theme.primary.normal};
+  cursor: pointer;
+`;
+
+export const CheckedIcon = styled(Checked)``;
+
+export const UncheckedBox = styled.div`
+  appearance: none;
+  width: 1.25rem;
+  height: 1.25rem;
+  border: 2px solid ${({ theme }) => theme.line.normal};
+  border-radius: 0.25rem;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.fill.normal};
+`;
+
+export const ErrorText = styled.span`
+  ${font.caption.medium};
+  color: ${palette.red[60]};
+  margin-top: 0.5rem;
+  display: block;
+`;
--- src/renderer/src/features/home/ui/rival/Rival.tsx
+++ src/renderer/src/features/home/ui/rival/Rival.tsx
@@ -1,9 +1,8 @@
 import * as S from "./Rival.style";
 import { MyRivalUsers } from "@/features/home/ui/rival/myrival-users/MyRivalUsers";
-import { Dialog } from "@/shared/ui";
 import { Link } from "react-router-dom";
 import { useRival } from "@/features/home/model/useRival";
-import { SearchInput } from "@/shared/ui/search-input";
+import { AddRivalsDialog } from "@/features/home/lib/AddRivals";
 
 export const Rival = () => {
   const getRivalData = useRival();
@@ -35,55 +34,7 @@ export const Rival = () => {
       </S.RivalBox>
 
       {getRivalData.modalOpen && (
-        <Dialog
-          title={"라이벌 추가"}
-          width={21.625}
-          height={25.175}
-          isOpen={getRivalData.modalOpen}
-          onClose={getRivalData.handleClose}
-        >
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div>
-              <SearchInput
-                placeholder={"이름 또는 아이디 검색"}
-                inputSize={"md"}
-                variant={"light"}
-                fullWidth={true}
-                style={{ margin: "1rem 0" }}
-              />
-              <S.UserChoiceContainer>
-                {getRivalData.userList?.users.map(user => (
-                  <S.UserChoiceBox
-                    key={user.id}
-                    $isSelected={getRivalData.rivalSelectedId.includes(user.id)}
-                    onClick={() => getRivalData.handleUserSelect(user.id)}
-                  >
-                    <S.ProfileContent style={{ height: "3rem" }}>
-                      <S.ProfileIcon />
-                      <S.ProfileTagBox>
-                        <S.ProfileName>{user.name}</S.ProfileName>
-                        <S.ProfileMention>@{user.username}</S.ProfileMention>
-                      </S.ProfileTagBox>
-                    </S.ProfileContent>
-
-                    {getRivalData.rivalSelectedId.includes(user.id) ? (
-                      <S.CheckedIcon />
-                    ) : (
-                      <S.UncheckedBox />
-                    )}
-                  </S.UserChoiceBox>
-                ))}
-              </S.UserChoiceContainer>
-            </div>
-          </div>
-          <S.BottomBox>
-            <S.ButtonBox>
-              <S.CloseButton onClick={getRivalData.handleClose}>취소</S.CloseButton>
-              <S.OkayButton onClick={getRivalData.handleRivalCreate}>확인</S.OkayButton>
-              {getRivalData.error && <S.ErrorText>{getRivalData.error}</S.ErrorText>}
-            </S.ButtonBox>
-          </S.BottomBox>
-        </Dialog>
+        <AddRivalsDialog isOpen={getRivalData.modalOpen} onClose={getRivalData.handleClose} />
       )}
     </S.RivalContainer>
   );
