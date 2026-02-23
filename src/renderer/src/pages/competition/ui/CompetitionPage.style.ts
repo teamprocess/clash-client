50152437aae4a4c2f262d8315b66a24c7dad3da4
오용준 <dhdydwns723@gmail.com>
2026-02-23 21:12:15 +0900

    fix: 나와의 경쟁, 라이벌 추가 ux

--- src/renderer/src/pages/competition/ui/CompetitionPage.style.ts
+++ src/renderer/src/pages/competition/ui/CompetitionPage.style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { font } from "@/shared/config/font";
+import Smile from "@/shared/ui/assets/sweating-smile-face.svg";
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -38,4 +39,43 @@ export const WitchCompete = styled.button<{ $isActive: boolean }>`
 export const EmptyText = styled.p`
   ${font.headline2.medium}
   color: ${({ theme }) => theme.label.neutral};
+`;
+
+export const PreDialogContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContentDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SmileIcon = styled(Smile)`
+  width: 4.5rem;
+  height: 4.5rem;
+`;
+
+export const ContentText = styled.p`
+  ${font.headline1.bold}
+  color: ${({ theme }) => theme.label.normal};
+`;
+
+export const ContentSubText = styled.p`
+  ${font.label.medium}
+  color: ${({ theme }) => theme.label.alternative};
 `;
