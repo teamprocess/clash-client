91317c14f52cd59165af31549b157620f5e7596b
오용준 <dhdydwns723@gmail.com>
2026-02-24 21:21:31 +0900

    refactor: 라이벌 추가 다이얼로그 UI 변경

--- src/renderer/src/features/home/lib/addRival/AddRivals.tsx
+++ src/renderer/src/features/home/lib/addRival/AddRivals.tsx
@@ -25,45 +25,61 @@ export const AddRivalsDialog = ({ isOpen, onClose, rival }: AddRivalsDialogProps
       isOpen={isOpen}
       onClose={handleClose}
     >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <SearchInput
-          placeholder={"이름 또는 아이디 검색"}
-          inputSize={"md"}
-          variant={"light"}
-          fullWidth={true}
-          style={{ margin: "1rem 0" }}
-          value={rival.searchText}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => rival.setSearchText(e.target.value)}
-        />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <SearchInput
+            placeholder={"이름 또는 아이디 검색"}
+            inputSize={"md"}
+            variant={"light"}
+            fullWidth={true}
+            style={{ margin: "1rem 0" }}
+            value={rival.searchText}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              rival.setSearchText(e.target.value)
+            }
+          />
+          <S.UserChoiceContainer>
+            {rival.filteredUsers.map(user => (
+              <S.UserChoiceBox
+                key={user.id}
+                $isSelected={rival.rivalSelectedId.includes(user.id)}
+                onClick={() => rival.handleUserSelect(user.id)}
+              >
+                <S.ProfileContent style={{ height: "3rem" }}>
+                  <S.ProfileIcon />
+                  <S.ProfileTagBox>
+                    <S.ProfileName>{user.name}</S.ProfileName>
+                    <S.ProfileMention>@{user.username}</S.ProfileMention>
+                  </S.ProfileTagBox>
+                </S.ProfileContent>
 
-        <S.UserChoiceContainer>
-          {rival.filteredUsers.map(user => (
-            <S.UserChoiceBox
-              key={user.id}
-              $isSelected={rival.rivalSelectedId.includes(user.id)}
-              onClick={() => rival.handleUserSelect(user.id)}
-            >
-              <S.ProfileContent style={{ height: "3rem" }}>
-                <S.ProfileIcon />
-                <S.ProfileTagBox>
-                  <S.ProfileName>{user.name}</S.ProfileName>
-                  <S.ProfileMention>@{user.username}</S.ProfileMention>
-                </S.ProfileTagBox>
-              </S.ProfileContent>
+                {rival.rivalSelectedId.includes(user.id) ? <S.CheckedIcon /> : <S.UncheckedBox />}
+              </S.UserChoiceBox>
+            ))}
+          </S.UserChoiceContainer>
+        </div>
 
-              {rival.rivalSelectedId.includes(user.id) ? <S.CheckedIcon /> : <S.UncheckedBox />}
-            </S.UserChoiceBox>
-          ))}
-        </S.UserChoiceContainer>
-
-        <S.BottomBox>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {rival.error && <S.ErrorText>{rival.error}</S.ErrorText>}
           <S.ButtonBox>
             <S.CloseButton onClick={rival.handleSelectClose}>초기화</S.CloseButton>
-
             <S.OkayButton onClick={rival.handleRivalCreate}>확인</S.OkayButton>
-            {rival.error && <S.ErrorText>{rival.error}</S.ErrorText>}
           </S.ButtonBox>
-        </S.BottomBox>
+        </div>
       </div>
     </Dialog>
   );
