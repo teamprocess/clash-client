3d436f8b0016c2158ef895507eafa68d4ae901cc
한승환 <coderrornotfound@gmail.com>
2026-02-25 13:41:12 +0900

    refactor: 섹션 ItemBox 컴포넌트화

--- src/renderer/src/features/section/ui/Section.tsx
+++ src/renderer/src/features/section/ui/Section.tsx
@@ -7,6 +7,7 @@ import { PreviewModal } from "@/features/section/components/PreviewModal";
 import { useMajorSectionQuery } from "@/entities/roadmap/section/api/query/useMajorSection.query";
 import { MajorEnum, section } from "@/entities/roadmap/section/model/section.types";
 import { useGetMyProfile } from "@/entities/user";
+import { SectionItemBox } from "../components/SectionItemBox";
 
 export const Section = () => {
   const navigate = useNavigate();
@@ -43,21 +44,12 @@ export const Section = () => {
       <S.RoadmapScrollable>
         <S.SectionItemWrapper>
           {sectionData?.categories.map(category => (
-            <S.SectionItemBox key={category}>
-              {sectionData.sections
-                .filter(item => item.category === category)
-                .map(item => (
-                  <S.SectionItem key={item.id} style={{ opacity: item.locked ? 0.5 : 1 }}>
-                    <S.SectionIconWrapper onClick={() => handleClick(item)}>
-                      {/*서버로부터 사진 연결 전 임시로 null*/}
-                      <S.SectionIcon src={"null"} />
-                      {item.completed && <S.SectionComplete />}
-                      {item.locked && <S.SectionLock />}
-                    </S.SectionIconWrapper>
-                    <S.SectionTitle>{item.title}</S.SectionTitle>
-                  </S.SectionItem>
-                ))}
-            </S.SectionItemBox>
+            <SectionItemBox
+              key={category}
+              category={category}
+              sections={sectionData.sections}
+              onItemClick={handleClick}
+            />
           ))}
         </S.SectionItemWrapper>
 
