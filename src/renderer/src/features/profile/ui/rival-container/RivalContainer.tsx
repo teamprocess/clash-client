00bd568a569b252432454592b8a86e00a0e4dea6
seunga418 <seungab77@gmail.com>
2026-02-27 20:57:21 +0900

    feat: 나의 라이벌 API 연동

--- src/renderer/src/features/profile/ui/rival-container/RivalContainer.tsx
+++ src/renderer/src/features/profile/ui/rival-container/RivalContainer.tsx
@@ -1,23 +1,23 @@
 import * as S from "./RivalContainer.style";
 import { useRivalContainer } from "../../model/useRivalContainer";
-import { RivalCard } from "./rival-card/RivalCard";
+import { RivalCard } from "@/features/profile";
 import { VscodeIcon } from "./RivalContainer.style";
 
 export const RivalContainer = () => {
-  const { rivals, maxRivals } = useRivalContainer();
-  const viewRivals = rivals.slice(0, maxRivals);
+  const { rivalsData } = useRivalContainer();
+  const rivals = rivalsData?.myRivals ?? [];
 
   return (
     <S.Container>
-      {viewRivals.map(rival => (
+      {rivals.map(rival => (
         <RivalCard
           key={rival.id}
           name={rival.name}
           status={rival.status}
-          time={rival.time}
-          appName={rival.appName}
-          profileSrc={S.profileSrcMap[rival.profileKey]}
-          appIconSrc={VscodeIcon[rival.appKey]}
+          time={rival.activeTime}
+          appName={rival.usingApp}
+          profileSrc={S.profileSrcMap["default"]}
+          appIconSrc={VscodeIcon["vscode"]}
         />
       ))}
     </S.Container>
