dc06604f27c7031a7f487b032ad5fff18ef4634c
한승환 <coderrornotfound@gmail.com>
2026-03-02 14:02:52 +0900

    refactor: 전공 다시 선택하기 기능 UI 구현

--- src/renderer/src/features/section/ui/Section.style.ts
+++ src/renderer/src/features/section/ui/Section.style.ts
@@ -1,9 +1,33 @@
 import styled from "styled-components";
+import { font } from "@/shared/config/font";
+import Change from "../assets/change.svg";
 
 export const RoadmapContainer = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+`;
+
+export const RoadmapTitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  position: absolute;
+  top: 1.8rem;
+  left: 2.5rem;
+  z-index: 120;
+  pointer-events: none;
+`;
+
+export const RoadmapTitle = styled.span`
+  ${font.title2.medium}
+  color: ${({ theme }) => theme.label.neutral};
+`;
+
+export const RoadmapTitleArrowIcon = styled(Change)`
+  color: ${({ theme }) => theme.label.neutral};
+  width: 1.75rem;
+  height: 1.75rem;
 `;
 
 export const RoadmapScrollable = styled.div`
