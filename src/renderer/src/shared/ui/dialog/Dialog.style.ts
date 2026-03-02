3f8fe276325e827acadd1b4b2d021caf1db5f3a3
오용준 <dhdydwns723@gmail.com>
2026-03-02 11:55:30 +0900

    refactor: 다이얼로그  fullWidth/Height 적용

--- src/renderer/src/shared/ui/dialog/Dialog.style.ts
+++ src/renderer/src/shared/ui/dialog/Dialog.style.ts
@@ -1,30 +1,48 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { font } from "@/shared/config/font";
 import ExitIconSvg from "@/shared/ui/assets/exit.svg";
 
 export const DialogOverlay = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
+  inset: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1000;
   width: 100%;
   height: 100%;
+  padding: 1rem;
   background: rgba(0, 0, 0, 0.5);
 `;
 
-export const DialogContainer = styled.div<{ $width: number; $height: number }>`
+export const DialogContainer = styled.div<{
+  $width?: number;
+  $height?: number;
+  $fullWidth?: boolean;
+  $fullHeight?: boolean;
+}>`
   padding: 1.25rem;
   border-radius: 1.25rem;
-  width: ${({ $width }) => $width}rem;
-  height: ${({ $height }) => $height}rem;
   background-color: ${({ theme }) => theme.background.normal};
   box-shadow: 0 0 7px 0 ${({ theme }) => theme.line.normal};
   position: relative;
   display: flex;
   flex-direction: column;
+
+  width: ${({ $width }) => $width}rem;
+  height: ${({ $height }) => $height}rem;
+
+  ${({ $fullWidth }) =>
+    $fullWidth &&
+    css`
+      width: 100%;
+    `}
+
+  ${({ $fullHeight }) =>
+    $fullHeight &&
+    css`
+      height: 100%;
+    `}
 `;
 
 export const DialogTitle = styled.p`
