f75022780450ca324f239be2bd50b9c5a35516d6
chaeyn <chaulfe@gmail.com>
2026-03-04 11:37:38 +0900

    refactor: env, svg 타입 선언 파일 분리

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -8,15 +8,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
-
-declare module "*.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
-
-declare module "*.svg?react" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
