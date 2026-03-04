d813ccc854a7d39d06d82a6e096ade458e374886
chaeyn <chaulfe@gmail.com>
2026-03-04 11:37:38 +0900

    refactor: env, svg 타입 선언 파일 분리

--- apps/web/src/svg.d.ts
+++ apps/web/src/svg.d.ts
@@ -1,9 +1,6 @@
-/// <reference types="vite/client" />
-
 declare module "*.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+  const src: string;
+  export default src;
 }
 
 declare module "*.svg?react" {
