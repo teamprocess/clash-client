21e4f75084a0be7d23b30cbf1c02659f5c63be42
chaeyn <chaulfe@gmail.com>
2026-03-08 17:54:48 +0900

    feat: 매일 오전 6시에 그룹원 재조회

--- apps/mac/src/renderer/src/features/app-monitor/index.ts
+++ apps/mac/src/renderer/src/features/app-monitor/index.ts
@@ -1,3 +1,4 @@
 export { useAppMonitor } from "./model/useAppMonitor";
+export { useDailyRefresh } from "./model/useDailyRefresh";
 export { useSidebarMonitor } from "./model/useSidebarMonitor";
 export { useRealtimeSync } from "./model/useRealtimeSync";
