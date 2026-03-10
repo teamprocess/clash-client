4e669ac03758f5522a9613eea9d120f2f7dbabf2
daehyeong2 <baconbacon1231@gmail.com>
2026-03-10 19:53:37 +0900

    feat: 상점 상품 로딩 뷰 추가

--- apps/mac/src/renderer/src/pages/shop/ShopPage.tsx
+++ apps/mac/src/renderer/src/pages/shop/ShopPage.tsx
@@ -2,9 +2,13 @@ import { MainProducts } from "@/features/shop/ui/main/MainProducts";
 import { useShop } from "@/features/shop/model/useShop";
 
 export const ShopPage = () => {
-  const { recommendedProducts, popularProducts } = useShop();
+  const { recommendedProducts, popularProducts, isLoading } = useShop();
 
   return (
-    <MainProducts recommendedProducts={recommendedProducts} popularProducts={popularProducts} />
+    <MainProducts
+      recommendedProducts={recommendedProducts}
+      popularProducts={popularProducts}
+      isLoading={isLoading}
+    />
   );
 };
