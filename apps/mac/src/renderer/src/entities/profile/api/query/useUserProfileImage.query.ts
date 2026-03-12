b1e50e4b18821f26c6b4f0464e4593eec500fa8a
오용준 <dhdydwns723@gmail.com>
2026-03-12 21:33:27 +0900

    feat: 프로필 이미지 업로드 URL 발급 api 호출

--- apps/mac/src/renderer/src/entities/profile/api/query/useUserProfileImage.query.ts
+++ apps/mac/src/renderer/src/entities/profile/api/query/useUserProfileImage.query.ts
@@ -1,9 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
-import { applyProfileImageApi } from "@/entities/profile/api/applyProfileImageApi";
+import { profileImageApi } from "@/entities/profile/api/applyProfileImageApi";
 import { ApplyProfileImageRequest } from "@/entities/profile/model/applyProfileImage.types";
 
 export const useApplyProfileImageMutation = () => {
   return useMutation({
-    mutationFn: (data: ApplyProfileImageRequest) => applyProfileImageApi.patchProfileImage(data),
+    mutationFn: (data: ApplyProfileImageRequest) => profileImageApi.patchProfileImage(data),
   });
 };
--- apps/mac/src/renderer/src/entities/profile/model/applyProfileImage.types.ts
+++ apps/mac/src/renderer/src/entities/profile/model/applyProfileImage.types.ts
@@ -1,3 +1,21 @@
+export interface PostProfileImagePresignedUrlRequest {
+  fileName: string;
+  contentType: string;
+}
+
+export interface PostProfileImagePresignedUrlResponse {
+  success: boolean;
+  message: string;
+  data: {
+    uploadUrl: string;
+    objectKey: string;
+    fileUrl: string;
+    httpMethod: "PUT";
+    contentType: string;
+    expiresInSeconds: number;
+  };
+}
+
 export interface ApplyProfileImageRequest {
   profileImageUrl: string;
 }
