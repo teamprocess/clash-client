26d94167ca34744d783bef5c16bb79922dd19dca
오용준 <dhdydwns723@gmail.com>
2026-03-12 21:33:27 +0900

    feat: 프로필 이미지 업로드 URL 발급 api 호출

--- apps/mac/src/renderer/src/entities/profile/api/query/userProfileImage.query.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { applyProfileImageApi } from "@/entities/profile/api/applyProfileImageApi";
-import { ApplyProfileImageRequest } from "@/entities/profile/model/applyProfileImage.types";
-
-export const useApplyProfileImageMutation = () => {
-  return useMutation({
-    mutationFn: (data: ApplyProfileImageRequest) => applyProfileImageApi.patchProfileImage(data),
-  });
-};
