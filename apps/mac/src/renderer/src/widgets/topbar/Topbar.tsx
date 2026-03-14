6f47ab58a955b223c368103aeb54338501e40ba9
오용준 <dhdydwns723@gmail.com>
2026-03-14 14:16:41 +0900

    feat: topbar 프로필 클릭 시 내 프로필, 로그아웃 표시

--- apps/mac/src/renderer/src/widgets/topbar/Topbar.tsx
+++ apps/mac/src/renderer/src/widgets/topbar/Topbar.tsx
@@ -1,7 +1,11 @@
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./Topbar.style";
 import { useGetMyProfile } from "@/entities/user";
 import { TopbarNotice } from "@/features/notice";
 import { formatPrice } from "@/shared/lib";
+import { Popover } from "@/shared/ui";
+import { useSignOut } from "@/features/auth";
 
 interface TopbarProps {
   onToggleSidebar: () => void;
@@ -9,34 +13,86 @@ interface TopbarProps {
 
 export const Topbar = ({ onToggleSidebar }: TopbarProps) => {
   const { data: user } = useGetMyProfile();
+  const { signOut } = useSignOut();
+  const navigate = useNavigate();
+
+  const profileMenuRef = useRef<HTMLButtonElement>(null);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+
+  const handleToggleProfileMenu = () => {
+    setIsProfileMenuOpen(prev => !prev);
+  };
+
+  const handleCloseProfileMenu = () => {
+    setIsProfileMenuOpen(false);
+  };
+
+  const handleMoveProfile = () => {
+    handleCloseProfileMenu();
+    navigate("/profile");
+  };
+
+  const handleLogout = () => {
+    handleCloseProfileMenu();
+    signOut();
+  };
 
   return (
     <S.TopbarContainer>
       <S.LeftMenu>
-        <S.MenuButton onClick={onToggleSidebar}>
+        <S.MenuButton type="button" onClick={onToggleSidebar}>
           <S.MenuIcon />
         </S.MenuButton>
         <S.LogoWrapper to="/">
           <S.Logo />
         </S.LogoWrapper>
       </S.LeftMenu>
+
       <S.RightMenu>
         <S.GoodsBox>
           <S.EXPIcon />
           <span>{formatPrice(user?.totalExp || 0)}</span>
         </S.GoodsBox>
+
         <S.GoodsBox>
           <S.CookieIcon />
           <span>{formatPrice(user?.totalCookie || 0)}</span>
         </S.GoodsBox>
+
         <TopbarNotice />
-        <S.ProfileBox to="/profile">
-          <S.ProfileIcon />
-          <S.NameBox>
-            <S.Name>{user?.name}</S.Name>
-            <S.Username>@{user?.username}</S.Username>
-          </S.NameBox>
-        </S.ProfileBox>
+
+        <S.ProfileMenuWrapper>
+          <S.ProfileButton
+            ref={profileMenuRef}
+            type="button"
+            onClick={handleToggleProfileMenu}
+            aria-haspopup="menu"
+            aria-expanded={isProfileMenuOpen}
+          >
+            <S.ProfileIcon />
+            <S.NameBox>
+              <S.Name>{user?.name}</S.Name>
+              <S.Username>@{user?.username}</S.Username>
+            </S.NameBox>
+          </S.ProfileButton>
+
+          <Popover
+            isOpen={isProfileMenuOpen}
+            onClose={handleCloseProfileMenu}
+            anchorRef={profileMenuRef}
+            align="end"
+            offset={10}
+          >
+            <S.MenuList>
+              <S.MenuItem type="button" $isLogout={false} onClick={handleMoveProfile}>
+                내 프로필
+              </S.MenuItem>
+              <S.MenuItem type="button" $isLogout onClick={handleLogout}>
+                로그아웃
+              </S.MenuItem>
+            </S.MenuList>
+          </Popover>
+        </S.ProfileMenuWrapper>
       </S.RightMenu>
     </S.TopbarContainer>
   );
