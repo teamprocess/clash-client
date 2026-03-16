9155a5b669010ea529c0f58a228821f07255b813
daehyeong2 <baconbacon1231@gmail.com>
2026-03-16 17:44:43 +0900

    feat: 상점 준비 중 안내 화면을 추가

--- apps/mac/src/renderer/src/pages/shop/products/ProductsPage.tsx
+++ apps/mac/src/renderer/src/pages/shop/products/ProductsPage.tsx
@@ -1,20 +1,5 @@
-import { Products } from "@/features/shop/ui/products/Products";
-import { useProducts } from "@/features/shop/model/useProducts";
+import { ShopComingSoon } from "../ShopComingSoon";
 
 export const ProductsPage = () => {
-  const { products, isLoading, keyword, setKeyword, sort, setSort, category, setCategory } =
-    useProducts();
-
-  return (
-    <Products
-      products={products?.products ?? []}
-      isLoading={isLoading}
-      keyword={keyword}
-      onKeywordChange={setKeyword}
-      sort={sort}
-      onSortChange={setSort}
-      category={category}
-      onCategoryChange={setCategory}
-    />
-  );
+  return <ShopComingSoon />;
 };
