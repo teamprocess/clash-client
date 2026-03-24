8ca3b31eb0eb4a2b0e80d66838dfe228bf1eadc1
chaeyn <chaulfe@gmail.com>
2026-03-24 12:09:15 +0900

    refactor: 전공 성향 검사 결과를 한국어로 통일

--- apps/mac/src/renderer/src/features/major-choice/ui/test/result/TestResult.tsx
+++ apps/mac/src/renderer/src/features/major-choice/ui/test/result/TestResult.tsx
@@ -1,22 +1,24 @@
 import * as S from "./TestResult.style";
 import { ResultProps } from "@/features/major-choice/model/useMajorChoice";
 import { Button } from "@/shared/ui/button";
+import { majorLabels } from "@/features/major-choice/model/major.constants";
 
 const majorIcons = {
-  Web: S.WebIcon,
-  Server: S.ServerIcon,
+  WEB: S.WebIcon,
+  SERVER: S.ServerIcon,
 };
 
 export const TestResult = ({ analyzedMajor, username, setStep }: ResultProps) => {
   const MajorIcon = majorIcons[analyzedMajor as keyof typeof majorIcons] || S.WebIcon;
+  const majorLabel = analyzedMajor ? majorLabels[analyzedMajor] : "";
 
   return (
     <S.ResultContainer>
       <S.ResultContents>
         <S.ResultContentsTop>
-          <S.ResultTitle>당신의 전공성향은 {analyzedMajor}입니다!</S.ResultTitle>
+          <S.ResultTitle>당신의 전공성향은 {majorLabel}입니다!</S.ResultTitle>
           <S.ResultDescription>
-            {username}님께 최적화된 전공 {analyzedMajor}를 찾았습니다.
+            {username}님께 최적화된 전공 {majorLabel}를 찾았습니다.
             <br />
             해당 분야의 로드맵을 확인하고 학습을 시작해보세요!
           </S.ResultDescription>
@@ -24,7 +26,7 @@ export const TestResult = ({ analyzedMajor, username, setStep }: ResultProps) =>
 
         <S.ResultCard>
           <MajorIcon />
-          <S.ResultMajor>{analyzedMajor}</S.ResultMajor>
+          <S.ResultMajor>{majorLabel}</S.ResultMajor>
         </S.ResultCard>
 
         <S.ButtonGroup>
