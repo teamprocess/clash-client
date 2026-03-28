413f48dbebc027e5892f97557eeedfb9f34f6f4c
daehyeong2 <baconbacon1231@gmail.com>
2026-03-29 03:46:27 +0900

    style: 레벨 슬라이더 hover 배경 전환과 thumb 색상을 조정

--- apps/mac/src/renderer/src/shared/ui/level-slider/level-slider-thumb/LevelSliderThumb.style.ts
+++ apps/mac/src/renderer/src/shared/ui/level-slider/level-slider-thumb/LevelSliderThumb.style.ts
@@ -14,7 +14,7 @@ export const LevelSliderThumbLine = styled.div<{
   // step이 증가할 수록 흰 색에서 primary color로 가까워 지도록 color-mix
   background-color: color-mix(
     in hsl shorter hue,
-    #ddd ${({ $step }) => `${100 - 12.5 * $step}%`},
+    #ccc ${({ $step }) => `${100 - 12.5 * $step}%`},
     ${({ theme }) => theme.primary.normal} ${({ $step }) => `${12.5 * $step}%`}
   );
   width: 100%;
