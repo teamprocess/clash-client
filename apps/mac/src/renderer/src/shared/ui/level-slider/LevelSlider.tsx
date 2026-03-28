ad7e865b466fa284c6ee69298c5868981f93b319
daehyeong2 <baconbacon1231@gmail.com>
2026-03-29 01:23:41 +0900

    feat: 레벨 슬라이더 hover 상태로 트랙과 라벨 연동

--- apps/mac/src/renderer/src/shared/ui/level-slider/LevelSlider.tsx
+++ apps/mac/src/renderer/src/shared/ui/level-slider/LevelSlider.tsx
@@ -1,16 +1,20 @@
 import * as S from "./LevelSlider.style";
 import { LevelSliderTrack } from "@/shared/ui/level-slider/level-slider-track/LevelSliderTrack";
 import { LevelSliderLabels } from "@/shared/ui/level-slider/level-slider-labels/LevelSliderLabels";
+import { useState } from "react";
+import { LEVEL_ENUM } from "@/shared/ui/level-slider/types";
 
 interface LevelSliderProps {
   labels: string[];
 }
 
 export const LevelSlider = ({ labels }: LevelSliderProps) => {
+  const [hoverLevel, setHoverLevel] = useState<LEVEL_ENUM | undefined>(undefined);
+  const selectedLevel = undefined;
   return (
-    <S.LevelSliderWrapper>
-      <LevelSliderTrack />
-      <LevelSliderLabels labels={labels} />
+    <S.LevelSliderWrapper onMouseLeave={() => setHoverLevel(undefined)}>
+      <LevelSliderTrack hoverLevel={hoverLevel} setHoverLevel={setHoverLevel} selectedLevel={selectedLevel} />
+      <LevelSliderLabels hoverLevel={hoverLevel} setHoverLevel={setHoverLevel} selectedLevel={selectedLevel} labels={labels} />
     </S.LevelSliderWrapper>
   );
 };
