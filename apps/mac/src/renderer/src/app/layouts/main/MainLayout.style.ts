09eb7cc70b228fe5d62461e3b449168d45b6e521
chaeyn <chaulfe@gmail.com>
2026-04-05 22:53:17 +0900

    fix: 스크롤 바로 인해 우측 패딩처럼 보이는 현상 해결

--- apps/mac/src/renderer/src/app/layouts/main/MainLayout.style.ts
+++ apps/mac/src/renderer/src/app/layouts/main/MainLayout.style.ts
@@ -16,6 +16,5 @@ export const MainContent = styled.main`
   flex: 1;
   padding: 2.5rem 2.5rem 2.5rem 2.5rem;
   overflow: auto;
-  scrollbar-gutter: stable;
   background-color: ${({ theme }) => theme.background.alternative};
 `;
