b93a34e03fd00a1388319e0899f0e05072bd8d52
daehyeong2 <baconbacon1231@gmail.com>
2026-04-21 10:49:43 +0900

    fix: 홈 페이지의 랭킹 툴팁 수정

--- apps/mac/src/renderer/src/features/home/ui/ranking/Ranking.constants.ts
+++ apps/mac/src/renderer/src/features/home/ui/ranking/Ranking.constants.ts
@@ -1,6 +1,6 @@
-export const rankingRewardTooltipContent = `Aura: 100,000 EXP & 상위 1등
-Master: 75,000 EXP & 상위 3등
-Diamond: 50,000 EXP
+export const rankingRewardTooltipContent = `Aura: 200,000 EXP & 상위 1등
+Master: 150,000 EXP & 상위 3등
+Diamond: 100,000 EXP
 Gold: 30,000 EXP
 Silver: 10,000 EXP
 Bronze: 1,000 EXP`;
